22b7745cb6d7613b9285736f4cff5fe02aebd1fa
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2012-06-13 22:16:09 +0200

    better caching of tth_bins

--- openCL/ocl_azim.pyx
+++ openCL/ocl_azim.pyx
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 #
-#    Project: Azimuthal integration 
+#    Project: Azimuthal integration
 #             https://forge.epn-campus.eu/projects/azimuthal
 #
 #    File: "$Id$"
@@ -25,7 +25,7 @@
 
 __author__ = "Jerome Kieffer"
 __license__ = "GPLv3"
-__date__ = "19/05/2012"
+__date__ = "13/06/2012"
 __copyright__ = "2012, ESRF, Grenoble"
 __contact__ = "jerome.kieffer@esrf.fr"
 
@@ -36,7 +36,7 @@ cimport numpy
 import numpy
 import threading
 import cython
-import time
+
 
 
 cdef class Integrator1d:
@@ -45,13 +45,14 @@ cdef class Integrator1d:
     """
     cdef ocl_xrpd1d.ocl_xrpd1D_fullsplit* cpp_integrator
     cdef char* _devicetype
-    cdef int _nBins, _nData, _platformid, _devid, _tth_id, _dtth_id, _mask_id, _solid_angle_id 
+    cdef int _nBins, _nData, _platformid, _devid, _tth_id, _dtth_id, _mask_id, _solid_angle_id
     cdef cpp_bool _useFp64
-    cdef float tth_min, tth_max
-#    cdef numpy.ndarray[numpy.float32_t, ndim = 1] _tth, _dtth, _mask, _solid_angle
-    
-    
+    cdef float tth_min, tth_max,_tth_min, _tth_max
+
     def __cinit__(self, filename=None):
+        """
+        Cython only contructor
+        """
         self._nBins = -1
         self._nData = -1
         self._useFp64 = False
@@ -63,11 +64,35 @@ cdef class Integrator1d:
         if filename is None:
             self.cpp_integrator = new ocl_xrpd1d.ocl_xrpd1D_fullsplit()
         else:
-            name = str(filename) 
+            name = str(filename)
             self.cpp_integrator = new ocl_xrpd1d.ocl_xrpd1D_fullsplit(name)
-            
+
+    def __init__(self,filename=None):
+        """
+        Python  constructor
+        """
+        self.filename = filename
+        self.tth_out = None
+
     def __dealloc__(self):
         del self.cpp_integrator
+
+    @cython.cdivision(True)
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    def _calc_tth_out(self,float lower, float upper):
+        """
+        Calculate the bin-center position in 2theta
+        """
+        self.tth_min = lower
+        self.tth_max = upper
+        cdef numpy.ndarray[numpy.float32_t, ndim = 1] tth_out = numpy.empty(self._nBins,dtype=numpy.float32)
+        cdef float delta = (upper - lower ) / (< float > (self._nBins))
+        with nogil:
+            for i in range(self._nBins):
+                tth_out[i] = self.tth_min + (0.5 +< float > i) * delta
+        self.tth_out = tth_out
+
 
     def getConfiguration(self, int Nimage, int Nbins, useFp64=None):
         """getConfiguration gets the description of the integrations to be performed and keeps an internal copy
@@ -91,10 +116,9 @@ cdef class Integrator1d:
         It also "ties" the OpenCL memory to the kernel arguments.
         If ANY of the arguments of getConfiguration needs to be changed, configure must
         be called again for them to take effect
-        
+
         @param kernel: name or path to the file containing the kernel
         """
-#        TODO
         kernel_name = "ocl_azim_kernel_2.cl"
         if kernel is None:
             if os.path.isfile(kernel_name):
@@ -103,39 +127,41 @@ cdef class Integrator1d:
                 kernel = os.path.join(os.path.dirname(os.path.abspath(__file__)),kernel_name)
         else:
             kernel = str(kernel)
-#        print kernel
-        #if non existant, look in installed path
         cdef char* ckernel = <char*> kernel
         cdef int rc
-#        with self._sem:
         with nogil:
                 rc = self.cpp_integrator.configure(ckernel)
         return rc
-    
-    def loadTth(self, numpy.ndarray tth not None, numpy.ndarray dtth not None, float tth_min, float tth_max):
+
+    def loadTth(self, numpy.ndarray tth not None, numpy.ndarray dtth not None, tth_min=None, tth_max=None):
         """Load the 2th arrays along with the min and max value.
         loadTth maybe be recalled at any time of the execution in order to update
         the 2th arrays.
-        
+
         loadTth is required and must be called at least once after a configure()
         """
         cdef int rc
         cdef numpy.ndarray[numpy.float32_t, ndim = 1]  tthc,dtthc
         tthc = numpy.ascontiguousarray(tth.ravel(),dtype=numpy.float32)
         dtthc = numpy.ascontiguousarray(dtth.ravel(),dtype=numpy.float32)
-#        if id(tthc) !=
-        self.tth_min=tth_min
-        self.tth_max=tth_max
-        with nogil:
-            rc=self.cpp_integrator.loadTth(<float*> tthc.data, <float*> dtthc.data, <float> tth_min, <float> tth_max)
-        return rc
 
+        self._tth_max=(tthc+dtthc).max()*(1.0 + numpy.finfo(numpy.float32).eps)
+        self._tth_min=(tthc-dtthc).min()
+        if tth_min is None:
+            tth_min = self._tth_min
+
+        if tth_max is None:
+            tth_max = self._tth_max
+        self._calc_tth_out(tth_min,tth_max)
+        with nogil:
+            rc=self.cpp_integrator.loadTth(<float*> tthc.data, <float*> dtthc.data, <float> self.tth_min, <float> self.tth_max)
+        return rc
 
     def setSolidAngle(self, numpy.ndarray solidAngle not None):
         """ Enables SolidAngle correction and uploads the suitable array to the OpenCL device.
          By default the program will assume no solidangle correction unless setSolidAngle() is called.
          From then on, all integrations will be corrected via the SolidAngle array.
-        
+
          If the SolidAngle array needs to be changes, one may just call setSolidAngle() again
          with that array
          @param solidAngle: numpy array representing the solid angle of the given pixel
@@ -143,7 +169,7 @@ cdef class Integrator1d:
          """
         cdef numpy.ndarray[numpy.float32_t, ndim = 1]  cSolidAngle
         cSolidAngle = numpy.ascontiguousarray(solidAngle.ravel(),dtype=numpy.float32)
-         
+
         return self.cpp_integrator.setSolidAngle(<float*> cSolidAngle.data)
 
     def unsetSolidAngle(self):
@@ -155,23 +181,23 @@ cdef class Integrator1d:
         return self.cpp_integrator.unsetSolidAngle()
 
     def setMask(self, numpy.ndarray mask not None):
-        """ 
+        """
         Enables the use of a Mask during integration. The Mask can be updated by
         recalling setMask at any point.
-        
+
         The Mask must be a PyFAI Mask. Pixels with 0 are masked out.
         @param mask: numpy.ndarray of integer.
-        @return integer 
+        @return integer
         """
         cdef numpy.ndarray[numpy.int_t, ndim = 1]  cMask
         if mask.dtype == numpy.int:
             cMask = numpy.ascontiguousarray(mask.ravel())
         else:
             cMask = numpy.ascontiguousarray(mask.astype(numpy.int).ravel())
-         
+
         return self.cpp_integrator.setMask(<int*> cMask.data)
 
-    def unsetMask(self): 
+    def unsetMask(self):
         """
         Disables the use of a Mask from that point.
         It may be re-enabled at any point via setMask
@@ -189,9 +215,9 @@ cdef class Integrator1d:
         with nogil:
             rc=self.cpp_integrator.setDummyValue(dummy,delta_dummy)
         return rc
-    
+
     def unsetDummyValue(self):
-        """Disable a dummy value. 
+        """Disable a dummy value.
         May be re-enabled at any time by setDummyValue
         """
         return self.cpp_integrator.unsetDummyValue()
@@ -199,62 +225,52 @@ cdef class Integrator1d:
     def setRange(self, float lowerBound, float upperBound):
         """Sets the active range to integrate on. By default the range is set to tth_min and tth_max
         By calling this functions, one may change to different bounds
-        
+
         @param lowerBound: usually tth_min
         @param upperBound: usually tth_max
-        @return: integer 
+        @return: integer
         """
-        self.tth_min = lowerBound
-        self.tth_max = upperBound
+        self._calc_tth_out(lowerBound, upperBound)
         return self.cpp_integrator.setRange(lowerBound, upperBound)
 
     def unsetRange(self):
         "Resets the 2th integration range back to tth_min, tth_max"
-        self.tth_min = None
-        self.tth_max = None
+        self._calc_tth_out(self._tth_min,self._tth_max)
         return self.cpp_integrator.unsetRange()
 
-    @cython.cdivision(True)
-    @cython.boundscheck(False)
-    @cython.wraparound(False)
+
     def execute(self, numpy.ndarray image not None):
         """Take an image, integrate and return the histogram and weights
         set / unset and loadTth methods have a direct impact on the execute() method.
         All the rest of the methods will require at least a new configuration via configure()"""
         cdef int rc,i
-        cdef numpy.ndarray[numpy.float32_t, ndim = 1] cimage, histogram,bins, outPos
+        cdef numpy.ndarray[numpy.float32_t, ndim = 1] cimage, histogram, bins
         cimage = numpy.ascontiguousarray(image.ravel(),dtype=numpy.float32)
         outPos = numpy.zeros(self._nBins,dtype=numpy.float32)
         histogram = numpy.zeros(self._nBins,dtype=numpy.float32)
         bins = numpy.zeros(self._nBins,dtype=numpy.float32)
         assert cimage.size == self._nData
-        cdef float delta = (self.tth_max - self.tth_min) / (< float > (self._nBins))
-        with nogil:    
-            for i in range(self._nBins):
-                outPos[i] = self.tth_min + (0.5 +< float > i) * delta
-#        t0=time.time()
         with nogil:
             rc = self.cpp_integrator.execute(<float*> cimage.data, <float*> histogram.data, <float*> bins.data)
-#        print("Executation time in Cython: %.3fms"%(1000*(time.time()-t0)))
         if rc!=0:
             raise RuntimeError("OpenCL integrator failed with RC=%s"%rc)
-        return outPos,histogram,bins
+        return self.tth_out,histogram,bins
 
     def clean(self, int preserve_context=0):
         """Free OpenCL related resources.
         It may be asked to preserve the context created by init or completely clean up OpenCL.
-        
+
         Guard / Status flags that are set will be reset. All the Operation flags are also reset"""
         return  self.cpp_integrator.clean(preserve_context)
 
 ################################################################################
-# Methods inherited from ocl_base class 
+# Methods inherited from ocl_base class
 ################################################################################
     def init(self,*args, **kwarg):
         """Initial configuration: Choose a device and initiate a context. Devicetypes can be GPU,gpu,CPU,cpu,DEF,ACC,ALL.
         Suggested are GPU,CPU. For each setting to work there must be such an OpenCL device and properly installed.
         E.g.: If Nvidia driver is installed, GPU will succeed but CPU will fail. The AMD SDK kit is required for CPU via OpenCL.
-        @param useFp64: boolean specifying if double precision will be used 
+        @param useFp64: boolean specifying if double precision will be used
         @param devicetype: string in ["cpu","gpu"]
         @param platformid: integer
         @param devid: integer
@@ -292,13 +308,13 @@ cdef class Integrator1d:
         """Same as show_devices but displays the results always on stdout even
         if the stream is set to a file"""
         self.cpp_integrator. print_devices()
-    
+
     def  show_device_details(self):
         "Print details of a selected device"
         self.cpp_integrator.show_device_details()
 
     def reset_time(self):
-        'Resets the internal profiling timers to 0'  
+        'Resets the internal profiling timers to 0'
         self.cpp_integrator.reset_time()
 
     def get_exec_time(self):
@@ -338,9 +354,9 @@ def histGPU1d(numpy.ndarray weights not None,
               ):
     """
     Calculates histogram of pos0 (tth) weighted by weights
-    
+
     Splitting is done on the pixel's bounding box like fit2D
-    
+
     @param weights: array with intensities
     @param pos0: 1D array with pos0: tth or q_vect
     @param delta_pos0: 1D array with delta pos0: max center-corner distance
@@ -349,7 +365,7 @@ def histGPU1d(numpy.ndarray weights not None,
     @param dummy: value for bins without pixels
     @param devicetype: "cpu" or "gpu" or "all"  or "def"
     @param useFp64: shall histogram be done in double precision (adviced)
-    @param platformid: platform number 
+    @param platformid: platform number
     @param deviceid: device number
     @return 2theta, I, unweighted histogram
     """
@@ -358,7 +374,7 @@ def histGPU1d(numpy.ndarray weights not None,
     assert delta_pos0.size == size
     assert  bins > 1
     cdef float pos0_min,pos0_max,pos0_maxin
-    
+
 
     if  (size,bins) not in _INTEGRATORS_1D:
         with lock:
@@ -384,7 +400,7 @@ def histGPU1d(numpy.ndarray weights not None,
                     rc = integr.init(devicetype, useFp64)
                 if rc!=0:
                     raise RuntimeError('Failed to initialize OpenCL deviceType %s (%s,%s) 64bits: %s'%(devicetype,platformid,deviceid,useFp64))
-                
+
                 if 0!= integr.getConfiguration(size, bins):
                     raise RuntimeError('Failed to configure 1D integrator with Ndata=%s and Nbins=%s'%(size,bins))
 
@@ -399,7 +415,7 @@ def histGPU1d(numpy.ndarray weights not None,
                     _INTEGRATORS_1D[(size,bins)]=integr
     integr = _INTEGRATORS_1D[(size,bins)]
     return integr.execute(weights)
-    
+
 def initocl_azim(*arg,**kwarg):
     """Place holder:
     It seams this must exist in this module to be valid"""
