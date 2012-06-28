bd35649857283b6b599c94079e58aa1964b92e22
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2012-06-28 21:53:51 +0200

    Should write some testing on the mask stuff

---
+++ test/testMask.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#    Project: Azimuthal integration
+#             https://forge.epn-campus.eu/projects/azimuthal
+#
+#    File: "$Id$"
+#
+#    Copyright (C) European Synchrotron Radiation Facility, Grenoble, France
+#
+#    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"test suite for masked arrays"
+
+__author__ = "Jérôme Kieffer"
+__contact__ = "Jerome.Kieffer@ESRF.eu"
+__license__ = "GPLv3+"
+__copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+__date__ = "28/06/2012"
+
+
+import unittest
+import os
+import numpy
+import logging, time
+import sys
+import fabio
+from utilstest import UtilsTest, Rwp, parseArgs
+logger = parseArgs(__file__)
+pyFAI = sys.modules["pyFAI"]
+from pyFAI.peakPicker import PeakPicker
+from pyFAI.geometryRefinement import GeometryRefinement
+
+if logger.getEffectiveLevel() <= logging.INFO:
+    import pylab
+
+class test_mask(unittest.TestCase):
+    pass
+
+#TODO
