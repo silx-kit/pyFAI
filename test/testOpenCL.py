563b4aa631fef99bf47a9dd15637df6dd4ceec08
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2012-07-03 00:03:08 +0200

    update tests

--- test/testOpenCL.py
+++ test/testOpenCL.py
@@ -44,21 +44,21 @@ pyFAI = sys.modules["pyFAI"]
 from pyFAI.ocl_azim import Integrator1d
 
 class test_mask(unittest.TestCase):
-    @classmethod
-    def find_device(cls):
-        fd, tmpfile = tempfile.mkstemp(suffix=".log", prefix="OpenCL-")
-        os.close(fd)
-        inte = Integrator1d(tmpfile)
-        for platform in range(10):
-            for device in range(10):
-                if not inte.init("all", platformid=platform, deviceid=device, useFp64=True):
-                    ids = inte.get_contexed_Ids()
-                    if ("cl_khr_int64_base_atomics" in inte.get_device_info()["extensions"]) or\
-                       ("NVIDIA CUDA" in inte.get_platform_info()["name"]):
-                        logger.info("Selected platform: %s device: %s" % (inte.get_platform_info()["name"],
-                                                                        inte.get_device_info()["name"]))
-                        return ids
-        return None
+#    @classmethod
+#    def find_device(cls):
+#        fd, tmpfile = tempfile.mkstemp(suffix=".log", prefix="OpenCL-")
+#        os.close(fd)
+#        inte = Integrator1d(tmpfile)
+#        for platform in range(10):
+#            for device in range(10):
+#                if not inte.init("all", platformid=platform, deviceid=device, useFp64=True):
+#                    ids = inte.get_contexed_Ids()
+#                    if ("cl_khr_int64_base_atomics" in inte.get_device_info()["extensions"]) or\
+#                       ("NVIDIA CUDA" in inte.get_platform_info()["name"]):
+#                        logger.info("Selected platform: %s device: %s" % (inte.get_platform_info()["name"],
+#                                                                        inte.get_device_info()["name"]))
+#                        return ids
+#        return None
 
     def setUp(self):
         self.datasets = [{"img":UtilsTest.getimage("1883/Pilatus1M.edf"), "poni":UtilsTest.getimage("1893/Pilatus1M.poni"), "spline": None},
@@ -73,20 +73,20 @@ class test_mask(unittest.TestCase):
                 spline = os.path.basename(ds["spline"])
                 open(ds["poni"], "w").write(data.replace(" " + spline, " " + ds["spline"]))
     def test_OpenCL(self):
-        ids = self.find_device()
+        ids = pyFAI.azimuthalIntegrator.ocl.select_device(extensions=["cl_khr_int64_base_atomics"])
         if ids is None:
             logger.error("No suitable OpenCL device found")
             return
         for ds in self.datasets:
             ai = pyFAI.load(ds["poni"])
             data = fabio.open(ds["img"]).data
-            ocl = ai.xrpd_OpenCL(data, 1000, platformid=ids[0], deviceid=ids[1], useFp64=True)
+            ocl = ai.xrpd_OpenCL(data, 1000, devicetype="all", platformid=ids[0], deviceid=ids[1], useFp64=True)
             t0 = time.time()
             ref = ai.xrpd(data, 1000)
             t1 = time.time()
-            ocl = ai.xrpd_OpenCL(data, 1000, platformid=ids[0], deviceid=ids[1], useFp64=True)
+            ocl = ai.xrpd_OpenCL(data, 1000, safe=False)
             t2 = time.time()
-            logger.info("For image %s; speed up is %.3fx; rate is %.3f Hz" % (os.path.basename(ds["img"]), ((t1 - t0) / (t2 - t1)), 1. / (t2 - t1)))
+            logger.info("For image %15s;\tspeed up is %.3fx;\trate is %.3f Hz" % (os.path.basename(ds["img"]), ((t1 - t0) / (t2 - t1)), 1. / (t2 - t1)))
             r = Rwp(ref, ocl)
             self.assertTrue(r < 6, "Rwp=%.3f for OpenCL processing of %s" % (r, ds))
 
