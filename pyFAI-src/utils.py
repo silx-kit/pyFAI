ab67edf04ea654660b0e9b043329dc3f3e1e1681
picca (diffabs) <picca@synchrotron-soleil.fr>
2013-09-27 10:44:49 +0200

    add the S140 detector

--- pyFAI-src/utils.py
+++ pyFAI-src/utils.py
@@ -904,3 +904,22 @@ def get_ui_file(filename):
 def get_cl_file(filename):
 #    return _get_data_path(os.path.join("openCL", filename))
     return _get_data_path(filename)
+
+
+class lazy_property(object):
+    '''
+    meant to be used for lazy evaluation of an object attribute.
+    property should represent non-mutable data, as it replaces itself.
+    '''
+
+    def __init__(self,fget):
+        self.fget = fget
+        self.func_name = fget.__name__
+
+
+    def __get__(self,obj,cls):
+        if obj is None:
+            return None
+        value = self.fget(obj)
+        setattr(obj,self.func_name,value)
+        return value
