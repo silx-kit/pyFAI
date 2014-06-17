66971a99fd6cb202396fc4b0e44c0515a724e4b0
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-06-17 15:25:03 +0200

    improved benchmarks for distortion

--- benchmark/benchmark_distortion.py
+++ benchmark/benchmark_distortion.py
@@ -4,9 +4,11 @@
 
 from __future__ import print_function, division
 
-import json, sys, time, timeit, os, platform, subprocess, gc
+import json, sys, time, timeit, os, platform, subprocess, gc, logging
+logging.basicConfig(level=logging.ERROR)
 import numpy
 import fabio
+
 import os.path as op
 sys.path.append(op.join(op.dirname(op.dirname(op.abspath(__file__))), "test"))
 import utilstest
@@ -34,14 +36,17 @@ number = 10
 
 print("Number of iteration: %s average over %s processing" % (repeat, number))
 for method in ("LUT","CSR"):
-    for device in ("None",(0,0),(0,1),(1,0),(2,0)):
-        setup = """
+    for device in ("None", '"CPU"', '"GPU"'):
+        for wg in [1, 2, 4, 8, 16, 32]:
+            if method != "CSR" and wg > 1:
+                continue
+            setup = """
 import pyFAI, pyFAI.distortion, numpy
 detector = pyFAI.detectors.FReLoN("%s")
-dis = pyFAI.distortion.Distortion(detector, method='%s', device=%s)
-data = numpy.random.random(detector.shape).astype(numpy.float32)
-dis.calc_init()""" % (splinefile, method, device)
-        t = timeit.Timer("dis.correct(data)", setup)
-        tmin = min([i / number for i in t.repeat(repeat=repeat, number=number)])
-        print("%s %s t=%.6fs" % (method, device, tmin))
-        gc.collect
+dis = pyFAI.distortion.Distortion(detector, method='%s', device=%s, workgroup=%s)
+data = numpy.random.randint(0,65000,size=detector.shape[0]*detector.shape[1]).reshape(detector.shape).astype(numpy.uint16)
+dis.calc_init()""" % (splinefile, method, device, wg)
+            t = timeit.Timer("dis.correct(data)", setup)
+            tmin = min([i / number for i in t.repeat(repeat=repeat, number=number)])
+            print("%s %s (wg=%2s) t=%.3fms" % (method, device, wg, tmin * 1000.0))
+            gc.collect
