8a29037ec978561c4f40d12f6655b58af98b7f18
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2014-06-27 23:18:32 +0200

    extract class Massif to its own file related to issue #116

--- pyFAI-src/peakPicker.py
+++ pyFAI-src/peakPicker.py
@@ -33,22 +33,19 @@ __status__ = "production"
 
 import os, sys, threading, logging, gc, types
 import operator
-from math                   import ceil, sqrt, pi
+from math import sqrt
 import numpy
-from scipy.optimize         import fmin
-from scipy.ndimage.filters  import median_filter
-from scipy.ndimage          import label
 from .gui_utils import pylab, update_fig, maximize_fig
 import fabio
-from .utils import gaussian_filter, binning, unBinning, deprecated, relabel, percentile
-from .bilinear import Bilinear
+from .utils import deprecated, percentile
 from .reconstruct import reconstruct
 from .calibrant import Calibrant, ALL_CALIBRANTS
 from .blob_detection import BlobDetection
+from .massif import Massif
 logger = logging.getLogger("pyFAI.peakPicker")
 if os.name != "nt":
     WindowsError = RuntimeError
-TARGET_SIZE = 1024
+
 
 
 ################################################################################
@@ -181,9 +178,9 @@ class PeakPicker(object):
         if self.fig and self.ax:
             #empty annotation and plots
             if len(self.ax.texts) > 0:
-               self.ax.texts = []
+                self.ax.texts = []
             if len(self.ax.lines) > 0:
-               self.ax.lines = []
+                self.ax.lines = []
             #Redraw the image
             self.fig.show()
             update_fig(self.fig)
@@ -794,242 +791,6 @@ class ControlPoints(object):
         self.calibrant.dSpacing = lst
     dSpacing = property(get_dSpacing, set_dSpacing)
 
-################################################################################
-# Massif
-################################################################################
-class Massif(object):
-    """
-    A massif is defined as an area around a peak, it is used to find neighbouring peaks
-    """
-    def __init__(self, data=None):
-        """
-
-        """
-        if isinstance(data, (str, unicode)) and os.path.isfile(data):
-            self.data = fabio.open(data).data.astype("float32")
-        elif  isinstance(data, fabio.fabioimage.fabioimage):
-            self.data = data.data.astype("float32")
-        else:
-            try:
-                self.data = data.astype("float32")
-            except Exception as error:
-                logger.error("Unable to understand this type of data %s: %s", data, error)
-        self._bilin = Bilinear(self.data)
-        self._blured_data = None
-        self._median_data = None
-        self._labeled_massif = None
-        self._number_massif = None
-        self._valley_size = None
-        self._binned_data = None
-        self.binning = None  # Binning is 2-list usually
-        self._sem = threading.Semaphore()
-        self._sem_label = threading.Semaphore()
-        self._sem_binning = threading.Semaphore()
-        self._sem_median = threading.Semaphore()
-
-
-    def nearest_peak(self, x):
-        """
-        @param x: coordinates of the peak
-        @returns the coordinates of the nearest peak
-        """
-#        x = numpy.array(x, dtype="float32")
-#        out = fmin(self._bilin.f_cy, x, disp=0).round().astype(numpy.int)
-        out = self._bilin.local_maxi(x)
-        if isinstance(out, tuple):
-            res = out
-        elif isinstance(out, numpy.ndarray):
-            res = tuple(out)
-        else:
-            res = [int(i) for idx, i in enumerate(out) if 0 <= i < self.data.shape[idx] ]
-        if (len(res) != 2) or not((0 <= out[0] < self.data.shape[0]) and (0 <= res[1] < self.data.shape[1])):
-            logger.error("in nearest_peak %s -> %s" % (x, out))
-            return
-        else:
-            return res
-
-
-    def calculate_massif(self, x):
-        """
-        defines a map of the massif around x and returns the mask
-        """
-        labeled = self.getLabeledMassif()
-        if labeled[x[0], x[1]] != labeled.max():
-            return (labeled == labeled[x[0], x[1]])
-
-
-    def find_peaks(self, x, nmax=200, annotate=None, massif_contour=None, stdout=sys.stdout):
-        """
-        All in one function that finds a maximum from the given seed (x)
-        then calculates the region extension and extract position of the neighboring peaks.
-        @param x: seed for the calculation, input coordinates
-        @param nmax: maximum number of peak per region
-        @param annotate: call back method taking number of points + coordinate as input.
-        @param massif_contour: callback to show the contour of a massif with the given index.
-        @param stdout: this is the file where output is written by default.
-        @return: list of peaks
-        """
-        listpeaks = []
-        region = self.calculate_massif(x)
-        if region is None:
-            logger.error("You picked a background point at %s", x)
-            return listpeaks
-        xinit = self.nearest_peak(x)
-        if xinit is None:
-            logger.error("Unable to find peak in the vinicy of %s", x)
-            return listpeaks
-        else:
-            if not region[int(xinit[0] + 0.5), int(xinit[1] + 0.5)]:
-                logger.error("Nearest peak %s is not in the same region  %s", xinit, x)
-                return listpeaks
-
-            if annotate is not None:
-                try:
-                    annotate(xinit, x)
-                except Exception as error:
-                    logger.error("Error in annotate %i: %i %i. %s" , len(listpeaks), xinit[0], xinit[1], error)
-
-        listpeaks.append(xinit)
-        mean = self.data[region].mean(dtype=numpy.float64)
-        region2 = region * (self.data > mean)
-        idx = numpy.vstack(numpy.where(region2)).T
-        numpy.random.shuffle(idx)
-        nmax = min(nmax, int(ceil(sqrt(idx.shape[0]))))
-        if massif_contour is not None:
-            try:
-                massif_contour(region)
-            except (WindowsError, MemoryError) as error:
-                logger.error("Error in plotting region: %s", error)
-        nbFailure = 0
-        for j in idx:
-            xopt = self.nearest_peak(j)
-            if xopt is None:
-                nbFailure += 1
-                continue
-            if (region2[xopt[0], xopt[1]]) and not (xopt in listpeaks):
-                stdout.write("[ %4i, %4i ] --> [ %5.1f, %5.1f ] after %3i iterations %s" % (tuple(j) + tuple(xopt) + (nbFailure, os.linesep)))
-                listpeaks.append(xopt)
-                nbFailure = 0
-            else:
-                nbFailure += 1
-            if (len(listpeaks) > nmax) or (nbFailure > 2 * nmax):
-                break
-        return listpeaks
-
-    def peaks_from_area(self, mask, Imin=None, keep=1000, **kwarg):
-        """
-        Return the list of peaks within an area
-
-        @param mask: 2d array with mask.
-        @param Imin: minimum of intensity above the background to keep the point
-        @param keep: maximum number of points to keep
-        @param kwarg: ignored parameters
-        @return: list of peaks [y,x], [y,x], ...]
-        """
-        all_points = numpy.vstack(numpy.where(mask)).T
-        res = []
-        cnt = 0
-        numpy.random.shuffle(all_points)
-        for idx in all_points:
-            out = self.nearest_peak(idx)
-            if out is not None:
-                print("[ %3i, %3i ] -> [ %.1f, %.1f ]" %
-                      (idx[1], idx[0], out[1], out[0]))
-                p0, p1 = int(out[0]), int(out[1])
-                if mask[p0, p1]:
-                    if (out not in res) and\
-                        (self.data[p0, p1] > Imin):
-                        res.append(out)
-                        cnt = 0
-            if len(res) >= keep or cnt > keep:
-                break
-            else:
-                cnt += 1
-        return res
-
-    def initValleySize(self):
-        if self._valley_size is None:
-            self.valley_size = max(5., max(self.data.shape) / 50.)
-
-
-    def getValleySize(self):
-        if self._valley_size is None:
-            self.initValleySize()
-        return self._valley_size
-    def setValleySize(self, size):
-        new_size = float(size)
-        if self._valley_size != new_size:
-            self._valley_size = new_size
-#            self.getLabeledMassif()
-            t = threading.Thread(target=self.getLabeledMassif)
-            t.start()
-    def delValleySize(self):
-        self._valley_size = None
-        self._blured_data = None
-    valley_size = property(getValleySize, setValleySize, delValleySize, "Defines the minimum distance between two massifs")
-
-    def getBinnedData(self):
-        """
-        @return binned data
-        """
-        if self._binned_data is None:
-            with self._sem_binning:
-                if self._binned_data is None:
-                    logger.info("Image size is %s", self.data.shape)
-                    self.binning = []
-                    for i in self.data.shape:
-                        if i % TARGET_SIZE == 0:
-                            self.binning.append(max(1, i // TARGET_SIZE))
-                        else:
-                            for j in range(i // TARGET_SIZE - 1, 0, -1):
-                                if i % j == 0:
-                                    self.binning.append(max(1, j))
-                                    break
-                            else:
-                                self.binning.append(1)
-#                    self.binning = max([max(1, i // TARGET_SIZE) for i in self.data.shape])
-                    logger.info("Binning size is %s", self.binning)
-                    self._binned_data = binning(self.data, self.binning)
-        return self._binned_data
-
-    def getMedianData(self):
-        if self._median_data is None:
-            with self._sem_median:
-                if self._median_data is None:
-                    self._median_data = median_filter(self.data, 3)
-                    if logger.getEffectiveLevel() == logging.DEBUG:
-                        fabio.edfimage.edfimage(data=self._median_data).write("median_data.edf")
-        return self._median_data
-
-    def getBluredData(self):
-        if self._blured_data is None:
-            with self._sem:
-                if self._blured_data is None:
-                    logger.debug("Blurring image with kernel size: %s" , self.valley_size)
-                    self._blured_data = gaussian_filter(self.getBinnedData(), [self.valley_size / i for i in  self.binning], mode="reflect")
-                    if logger.getEffectiveLevel() == logging.DEBUG:
-                        fabio.edfimage.edfimage(data=self._blured_data).write("blured_data.edf")
-        return self._blured_data
-
-    def getLabeledMassif(self, pattern=None):
-        if self._labeled_massif is None:
-            with self._sem_label:
-                if self._labeled_massif is None:
-                    if pattern is None:
-                        pattern = [[1] * 3] * 3  # [[0, 1, 0], [1, 1, 1], [0, 1, 0]]#[[1] * 3] * 3
-                    logger.debug("Labeling all massifs. This takes some time !!!")
-                    labeled_massif, self._number_massif = label((self.getBinnedData() > self.getBluredData()), pattern)
-                    logger.info("Labeling found %s massifs." % self._number_massif)
-                    if logger.getEffectiveLevel() == logging.DEBUG:
-                        fabio.edfimage.edfimage(data=labeled_massif).write("labeled_massif_small.edf")
-                    relabeled = relabel(labeled_massif, self.getBinnedData(), self.getBluredData())
-                    if logger.getEffectiveLevel() == logging.DEBUG:
-                            fabio.edfimage.edfimage(data=relabeled).write("relabeled_massif_small.edf")
-                    self._labeled_massif = unBinning(relabeled, self.binning, False)
-                    if logger.getEffectiveLevel() == logging.DEBUG:
-                        fabio.edfimage.edfimage(data=self._labeled_massif).write("labeled_massif.edf")
-                    logger.info("Labeling found %s massifs." % self._number_massif)
-        return self._labeled_massif
 
 
 class Event(object):
