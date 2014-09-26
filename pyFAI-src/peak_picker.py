724cfe3af9ca48946d8ff546375ef56b22cba393
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-09-26 18:45:25 +0200

    no op

--- pyFAI-src/peak_picker.py
+++ pyFAI-src/peak_picker.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 #    Project: Azimuthal integration
-#             https://forge.epn-campus.eu/projects/azimuthal
-#
-#    File: "$Id$"
+#             https://github.com/kif/pyFAI
 #
 #    Copyright (C) European Synchrotron Radiation Facility, Grenoble, France
 #
@@ -28,7 +26,7 @@ __author__ = "Jérôme Kieffer"
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "GPLv3+"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "22/09/2014"
+__date__ = "26/09/2014"
 __status__ = "production"
 
 import os
