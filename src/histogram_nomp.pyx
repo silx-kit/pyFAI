6fb64b7ad20c59ff61268aacf93228165e40c0c7
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2014-09-30 22:22:29 +0200

    remove anything related to pixel splitting from histogram. Other algorithms exist and they are more powerful !

--- src/histogram_nomp.pyx
+++ src/histogram_nomp.pyx
@@ -31,9 +31,11 @@ import cython
 import numpy
 cimport numpy
 import sys
-
+import logging
+logger = logging.getLogger("pyFAI.histogram_omp")
 from libc.math cimport floor
 EPS32 = (1.0 + numpy.finfo(numpy.float32).eps)
+
 
 @cython.cdivision(True)
 cdef inline double getBinNr(double x0, double pos0_min, double delta) nogil:
@@ -62,7 +64,7 @@ def histogram(numpy.ndarray pos not None, \
     @param pos: 2Theta array
     @param weights: array with intensities
     @param bins: number of output bins
-    @param pixelSize_in_Pos: size of a pixels in 2theta
+    @param pixelSize_in_Pos: size of a pixels in 2theta: DESACTIVATED
     @param nthread: OpenMP is disabled. unused
 
     @return 2theta, I, weighted histogram, raw histogram
@@ -76,82 +78,45 @@ def histogram(numpy.ndarray pos not None, \
         float[:] cdata = numpy.ascontiguousarray(weights.ravel(), dtype=numpy.float32)
         numpy.ndarray[numpy.float64_t, ndim = 1] out_data = numpy.zeros(bins, dtype="float64")
         numpy.ndarray[numpy.float64_t, ndim = 1] out_count = numpy.zeros(bins, dtype="float64")
-        numpy.ndarray[numpy.float64_t, ndim = 1] out_merge = numpy.empty(bins, dtype="float64")
-        numpy.ndarray[numpy.float64_t, ndim = 1] out_pos = numpy.empty(bins, dtype="float64")
-        double bin_edge_min, bin_edge_max
-        double delta
+        numpy.ndarray[numpy.float64_t, ndim = 1] out_merge = numpy.zeros(bins, dtype="float64")
+        double delta, min0, max0
         double a = 0.0
         double d = 0.0
         double fbin = 0.0
-        double ffbin = 0.0
-        double dInt = 0.0
-        double dIntR = 0.0
-        double dIntL = 0.0
-        double dtmp = 0.0
-        double dbin, inv_dbin2 = 0.0
         double epsilon = 1e-10
-        int bin = 0
-        int i, idx, t
+        int bin = 0, i, idx
+    if pixelSize_in_Pos:
+        logger.warning("No pixel splitting in histogram")
 
     if bin_range is not None:
-        bin_edge_min = bin_range[0]
-        bin_edge_max = bin_range[1] * EPS32
+        min0 = min(bin_range)
+        max0 = max(bin_range) * EPS32
     else:
-        bin_edge_min = pos.min()
-        bin_edge_max = pos.max() * EPS32
-    
-    delta = (bin_edge_max - bin_edge_min) / float(bins)
-    
-    if pixelSize_in_Pos is None:
-        dbin = 0.5
-        inv_dbin2 = 4.0
-    elif isinstance(pixelSize_in_Pos, (int, float)):
-        dbin = 0.5 * pixelSize_in_Pos / delta
-        if dbin > 0.0:
-            inv_dbin2 = 1 / dbin / dbin
-        else:
-            inv_dbin2 = 0.0
-    elif isinstance(pixelSize_in_Pos, numpy.ndarray):
-        pass #TODO
+        min0 = pos.min()
+        max0 = pos.max() * EPS32
 
-    if numpy.isnan(dbin) or numpy.isnan(inv_dbin2):
-        dbin = 0.0
-        inv_dbin2 = 0.0
+    delta = (max0 - min0) / float(bins)
+
 
     with nogil:
         for i in range(size):
             d = cdata[i]
             a = cpos[i]
-            if (a < bin_edge_min) or (a > bin_edge_max):
+            fbin = getBinNr(a, min0, delta)
+            bin = < int > fbin
+            if bin<0 or bin>= bins:
                 continue
-            fbin = getBinNr(a, bin_edge_min, delta)
-            ffbin = floor(fbin)
-            bin = < int > ffbin
-            dInt = 1.0
-            if bin > 0:
-                dtmp = ffbin - (fbin - dbin)
-                if dtmp > 0:
-                    dIntL = 0.5 * dtmp * dtmp * inv_dbin2
-                    dInt = dInt - dIntL
-                    out_count[bin - 1] += dIntL
-                    out_data[bin - 1] += d * dIntL
-
-            if bin < bins - 1:
-                dtmp = fbin + dbin - ffbin - 1
-                if dtmp > 0:
-                    dIntR = 0.5 * dtmp * dtmp * inv_dbin2
-                    dInt = dInt - dIntR
-                    out_count[bin + 1] += dIntR
-                    out_data[bin + 1] += d * dIntR
-            out_count[bin] += dInt
-            out_data[bin] += d * dInt
+            out_count[bin] += 1.0
+            out_data[bin] += d
 
         for idx in range(bins):
-            out_pos[idx] = bin_edge_min + (0.5 + idx) * delta
             if out_count[idx] > epsilon:
                 out_merge[idx] = out_data[idx] / out_count[idx]
             else:
                 out_merge[idx] = dummy
+
+    out_pos = numpy.linspace(min0 + (0.5 * delta), max0 - (0.5 * delta), bins)
+
     return out_pos, out_merge, out_data, out_count
 
 
@@ -162,7 +127,7 @@ def histogram2d(numpy.ndarray pos0 not None,
                 numpy.ndarray pos1 not None,
                 bins not None,
                 numpy.ndarray weights not None,
-                split=True,
+                split=False,
                 nthread=None,
                 float dummy=0.0):
     """
@@ -172,45 +137,45 @@ def histogram2d(numpy.ndarray pos0 not None,
     @param pos1: Chi array
     @param weights: array with intensities
     @param bins: number of output bins int or 2-tuple of int
+	@param split: pixel splitting is disabled in histogram
     @param nthread: OpenMP is disabled. unused
 
     @return  I, edges0, edges1, weighted histogram(2D), unweighted histogram (2D)
     """
     assert pos0.size == pos1.size
-#    if weights is not No:
     assert pos0.size == weights.size
     cdef:
-        int  bin0, bin1, i, j, b0, b1
+        int  bins0, bins1, i, j, bin0, bin1
         int  size = pos0.size
     try:
-        bin0, bin1 = tuple(bins)
+        bins0, bins1 = tuple(bins)
     except:
-        bin0 = bin1 = < int > bins
-    if bin0 <= 0:
-        bin0 = 1
-    if bin1 <= 0:
-        bin1 = 1
+        bins0 = bins1 = int(bins)
+    if bins0 <= 0:
+        bins0 = 1
+    if bins1 <= 0:
+        bins1 = 1
     cdef:
-        int csplit = split
         float[:] cpos0 = numpy.ascontiguousarray(pos0.ravel(), dtype=numpy.float32)
         float[:] cpos1 = numpy.ascontiguousarray(pos1.ravel(), dtype=numpy.float32)
         float[:] data = numpy.ascontiguousarray(weights.ravel(), dtype=numpy.float32)
-        numpy.ndarray[numpy.float64_t, ndim = 2] out_data = numpy.zeros((bin0, bin1), dtype="float64")
-        numpy.ndarray[numpy.float64_t, ndim = 2] out_count = numpy.zeros((bin0, bin1), dtype="float64")
-        numpy.ndarray[numpy.float64_t, ndim = 2] out_merge = numpy.empty((bin0, bin1), dtype="float64")
-        numpy.ndarray[numpy.float64_t, ndim = 1] edges0, edges1
+        numpy.ndarray[numpy.float64_t, ndim = 2] out_data = numpy.zeros((bins0, bins1), dtype="float64")
+        numpy.ndarray[numpy.float64_t, ndim = 2] out_count = numpy.zeros((bins0, bins1), dtype="float64")
+        numpy.ndarray[numpy.float64_t, ndim = 2] out_merge = numpy.zeros((bins0, bins1), dtype="float64")
         double min0 = pos0.min()
         double max0 = pos0.max() * EPS32
         double min1 = pos1.min()
         double max1 = pos1.max() * EPS32
-        double delta0 = (max0 - min0) / float(bin0) 
-        double delta1 = (max1 - min1) / float(bin1)  
-        double dbin0 = 0.5, dbin1 = 0.5
-        double fbin0, fbin1, p0, p1, d, rest, delta0l, delta0r, delta1l, delta1r, aera
+        double delta0 = (max0 - min0) / float(bins0)
+        double delta1 = (max1 - min1) / float(bins1)
+        double fbin0, fbin1, p0, p1, d
         double epsilon = 1e-10
 
-    edges0 = numpy.linspace(min0 + (0.5 * delta0), max0 - (0.5 * delta0), bin0)
-    edges1 = numpy.linspace(min1 + (0.5 * delta1), max1 - (0.5 * delta1), bin1)
+    if split:
+        logger.warning("No pixel splitting in histogram")
+
+    edges0 = numpy.linspace(min0 + (0.5 * delta0), max0 - (0.5 * delta0), bins0)
+    edges1 = numpy.linspace(min1 + (0.5 * delta1), max1 - (0.5 * delta1), bins1)
     with nogil:
         for i in range(size):
             p0 = cpos0[i]
@@ -218,94 +183,15 @@ def histogram2d(numpy.ndarray pos0 not None,
             d = data[i]
             fbin0 = getBinNr(p0, min0, delta0)
             fbin1 = getBinNr(p1, min1, delta1)
-            b0 = < int > floor(fbin0)
-            b1 = < int > floor(fbin1)
-            if b0 == bin0:
-                b0 = bin0 - 1
-                fbin0 = (< double > bin0) - 0.5
-            elif b0 == 0:
-                fbin0 = 0.5
-            if b1 == bin1:
-                b1 = bin1 - 1
-                fbin1 = (< double > bin1) - 0.5
-            elif b1 == 0:
-                fbin1 = 0.5
+            bin0 = < int > floor(fbin0)
+            bin1 = < int > floor(fbin1)
+            if (bin0<0) or (bin1<0) or (bin0>=bins0) or (bin1>=bins1):
+                continue
+            out_count[bin0, bin1] += 1.0
+            out_data[bin0, bin1] += d
 
-            rest = 1.0
-            if csplit == 1:
-                delta0l = fbin0 - < double > b0 - dbin0
-                delta0r = fbin0 - < double > b0 - 1 + dbin0
-                delta1l = fbin1 - < double > b1 - dbin1
-                delta1r = fbin1 - < double > b1 - 1 + dbin1                
-                if delta0l < 0 and b0 > 0:
-                    if delta1l < 0 and b1 > 0:
-                        area = delta0l * delta1l
-                        rest -= area
-                        out_count[b0 - 1, b1 - 1] += area
-                        out_data[b0 - 1, b1 - 1] += area * d
-
-                        area = (-delta0l) * (1 + delta1l)
-                        rest -= area
-                        out_count[b0 - 1, b1] += area
-                        out_data[b0 - 1, b1] += area * d
-
-                        area = (1 + delta0l) * (-delta1l)
-                        rest -= area
-                        out_count[b0, b1 - 1] += area
-                        out_data[b0, b1 - 1] += area * d
-
-                    elif delta1r > 0 and b1 < bin1 - 1:
-                        area = -delta0l * delta1r
-                        rest -= area
-                        out_count[b0 - 1, b1 + 1] += area
-                        out_data[b0 - 1, b1 + 1] += area * d
-
-                        area = (-delta0l) * (1 - delta1r)
-                        rest -= area
-                        out_count[b0 - 1, b1] += area
-                        out_data[b0 - 1, b1] += area * d
-
-                        area = (1 + delta0l) * (delta1r)
-                        rest -= area
-                        out_count[b0, b1 + 1] += area
-                        out_data[b0, b1 + 1] += area * d
-                elif delta0r > 0 and b0 < bin0 - 1:
-                    if delta1l < 0 and b1 > 0:
-                        area = -delta0r * delta1l
-                        rest -= area
-                        out_count[b0 + 1, b1 - 1] += area
-                        out_data[b0 + 1, b1 - 1] += area * d
-
-                        area = (delta0r) * (1 + delta1l)
-                        rest -= area
-                        out_count[b0 + 1, b1] += area
-                        out_data[b0 + 1, b1] += area * d
-
-                        area = (1 - delta0r) * (-delta1l)
-                        rest -= area
-                        out_count[b0, b1 - 1] += area
-                        out_data[b0, b1 - 1] += area * d
-
-                    elif delta1r > 0 and b1 < bin1 - 1:
-                        area = delta0r * delta1r
-                        rest -= area
-                        out_count[b0 + 1, b1 + 1] += area
-                        out_data[b0 + 1, b1 + 1] += area * d
-
-                        area = (delta0r) * (1 - delta1r)
-                        rest -= area
-                        out_count[b0 + 1, b1] += area
-                        out_data[b0 + 1, b1] += area * d
-
-                        area = (1 - delta0r) * (delta1r)
-                        rest -= area
-                        out_count[b0, b1 + 1] += area
-                        out_data[b0, b1 + 1] += area * d
-            out_count[b0, b1] += rest
-            out_data[b0, b1] += d * rest
-
-        for i in range(bin0):
-            for j in range(bin1):
+        for i in range(bins0):
+            for j in range(bins1):
                 if out_count[i, j] > epsilon:
                     out_merge[i, j] = out_data[i, j] / out_count[i, j]
                 else:
--- src/histogram_omp.pyx
+++ src/histogram_omp.pyx
@@ -24,7 +24,7 @@
 #
 
 __author__ = "Jerome Kieffer"
-__date__ = "29/09/2014" 
+__date__ = "29/09/2014"
 __name__ = "histogram"
 
 import cython
@@ -33,7 +33,8 @@ import numpy
 from cython.view cimport array as cvarray
 cimport numpy
 import sys
-
+import logging
+logger = logging.getLogger("pyFAI.histogram_omp")
 from libc.math cimport floor
 from openmp cimport omp_set_num_threads, omp_get_max_threads, omp_get_thread_num
 EPS32 = (1.0 + numpy.finfo(numpy.float32).eps)
@@ -82,31 +83,25 @@ def histogram(numpy.ndarray pos not None, \
         numpy.ndarray[numpy.float64_t, ndim = 1] out_data = numpy.zeros(bins, dtype="float64")
         numpy.ndarray[numpy.float64_t, ndim = 1] out_count = numpy.zeros(bins, dtype="float64")
         numpy.ndarray[numpy.float64_t, ndim = 1] out_merge = numpy.zeros(bins, dtype="float64")
-        numpy.ndarray[numpy.float64_t, ndim = 1] out_pos = numpy.zeros(bins, dtype="float64")
-        double delta, bin_edge_min, bin_edge_max
+        double delta, min0, max0
         double a = 0.0
         double d = 0.0
         double fbin = 0.0
-        double ffbin = 0.0
-        double dInt = 0.0
-        double dIntR = 0.0
-        double dIntL = 0.0
-        double dtmp = 0.0
-        double dbin, inv_dbin2 = 0.0
         double tmp_count, tmp_data = 0.0
         double epsilon = 1e-10
-        int bin = 0, thread
-        int i, t, dest = 0
+        int bin = 0, i, idx, thread
+    if pixelSize_in_Pos:
+        logger.warning("No pixel splitting in histogram")
 
     if bin_range is not None:
-        bin_edge_min = bin_range[0]
-        bin_edge_max = bin_range[1] * EPS32
+        min0 = min(bin_range)
+        max0 = max(bin_range) * EPS32
     else:
-        bin_edge_min = pos.min()
-        bin_edge_max = pos.max() * EPS32
-    
-    delta = (bin_edge_max - bin_edge_min) / float(bins)
-    
+        min0 = pos.min()
+        max0 = pos.max() * EPS32
+
+    delta = (max0 - min0) / float(bins)
+
     if nthread is not None:
         if isinstance(nthread, int) and (nthread > 0):
             omp_set_num_threads(< int > nthread)
@@ -121,67 +116,35 @@ def histogram(numpy.ndarray pos not None, \
     big_count[:, :] = 0.0
     big_data[:, :] = 0.0
 
-    if pixelSize_in_Pos is None:
-        dbin = 0.5
-        inv_dbin2 = 4.0
-    elif isinstance(pixelSize_in_Pos, (int, float)):
-        dbin = 0.5 * pixelSize_in_Pos / delta
-        if dbin > 0.0:
-            inv_dbin2 = 1 / dbin / dbin
-        else:
-            inv_dbin2 = 0.0
-    elif isinstance(pixelSize_in_Pos, numpy.ndarray):
-        pass #TODO
-
-    if numpy.isnan(dbin) or numpy.isnan(inv_dbin2):
-        dbin = 0.0
-        inv_dbin2 = 0.0
+    if pixelSize_in_Pos:
+        logger.warning("No pixel splitting in histogram")
 
     with nogil:
         for i in prange(size):
             d = cdata[i]
             a = cpos[i]
-            if (a < bin_edge_min) or (a > bin_edge_max):
+            fbin = getBinNr(a, min0, delta)
+            bin = < int > fbin
+            if bin<0 or bin>= bins:
                 continue
-            fbin = getBinNr(a, bin_edge_min, delta)
-            ffbin = floor(fbin)
-            bin = < int > ffbin
             thread = omp_get_thread_num()
-            dInt = 1.0
-            if bin > 0:
-                dtmp = ffbin - (fbin - dbin)
-                if dtmp > 0:
-                    dIntL = 0.5 * dtmp * dtmp * inv_dbin2
-                    dInt = dInt - dIntL
-                    big_count[thread, bin - 1] += dIntL
-                    big_data[thread, bin - 1] += d * dIntL
+            big_count[thread, bin] += 1.0
+            big_data[thread, bin] += d
 
-            if bin < bins - 1:
-                dtmp = fbin + dbin - ffbin - 1
-                if dtmp > 0:
-                    dIntR = 0.5 * dtmp * dtmp * inv_dbin2
-                    dInt = dInt - dIntR
-                    big_count[thread, bin + 1] += dIntR
-                    big_data[thread, bin + 1] += d * dIntR
-            big_count[thread, bin] += dInt
-            big_data[thread, bin] += d * dInt
-#            big_count[thread, bin] += 1.0
-#            big_data[thread, bin] += d 
-
-        for bin in prange(bins):
-            # Nota: += is for cython reduction only !
-            out_pos[bin] += bin_edge_min + (0.5 +< double > bin) * delta
+        for idx in prange(bins):
             tmp_count = 0.0
             tmp_data = 0.0
             for thread in range(omp_get_max_threads()):
-                tmp_count = tmp_count + big_count[thread, bin]
-                tmp_data = tmp_data + big_data[thread, bin]
-            out_count[bin] += tmp_count
-            out_data[bin] += tmp_data
-            if out_count[bin] > epsilon:
-                out_merge[bin] += tmp_data / tmp_count
+                tmp_count = tmp_count + big_count[thread, idx]
+                tmp_data = tmp_data + big_data[thread, idx]
+            out_count[idx] += tmp_count
+            out_data[idx] += tmp_data
+            if out_count[idx] > epsilon:
+                out_merge[idx] += tmp_data / tmp_count
             else:
-                out_merge[bin] += dummy
+                out_merge[idx] += dummy
+
+    out_pos = numpy.linspace(min0 + (0.5 * delta), max0 - (0.5 * delta), bins)
 
     return out_pos, out_merge, out_data, out_count
 
@@ -193,7 +156,7 @@ def histogram2d(numpy.ndarray pos0 not None,
                 numpy.ndarray pos1 not None,
                 bins not None,
                 numpy.ndarray weights not None,
-                split=True,
+                split=False,
                 nthread=None,
                 float dummy=0.0):
     """
@@ -203,6 +166,7 @@ def histogram2d(numpy.ndarray pos0 not None,
     @param pos1: Chi array
     @param weights: array with intensities
     @param bins: number of output bins int or 2-tuple of int
+	@param split: pixel splitting is disabled in histogram
     @param nthread: maximum number of thread to use. By default: maximum available.
     One can also limit this with OMP_NUM_THREADS environment variable
 
@@ -210,40 +174,39 @@ def histogram2d(numpy.ndarray pos0 not None,
     @return  I, edges0, edges1, weighted histogram(2D), unweighted histogram (2D)
     """
     assert pos0.size == pos1.size
-#    if weights is not No:
     assert pos0.size == weights.size
     cdef:
-        int  bin0, bin1, i, j, b0, b1
+        int  bins0, bins1, i, j, bin0, bin1
         int  size = pos0.size
     try:
-        bin0, bin1 = tuple(bins)
+        bins0, bins1 = tuple(bins)
     except:
-        bin0 = bin1 = < int > bins
-    if bin0 <= 0:
-        bin0 = 1
-    if bin1 <= 0:
-        bin1 = 1
+        bins0 = bins1 = int(bins)
+    if bins0 <= 0:
+        bins0 = 1
+    if bins1 <= 0:
+        bins1 = 1
     cdef:
-        int csplit = split
         float[:] cpos0 = numpy.ascontiguousarray(pos0.ravel(), dtype=numpy.float32)
         float[:] cpos1 = numpy.ascontiguousarray(pos1.ravel(), dtype=numpy.float32)
         float[:] data = numpy.ascontiguousarray(weights.ravel(), dtype=numpy.float32)
-        numpy.ndarray[numpy.float64_t, ndim = 2] out_data = numpy.zeros((bin0, bin1), dtype="float64")
-        numpy.ndarray[numpy.float64_t, ndim = 2] out_count = numpy.zeros((bin0, bin1), dtype="float64")
-        numpy.ndarray[numpy.float64_t, ndim = 2] out_merge = numpy.zeros((bin0, bin1), dtype="float64")
-        numpy.ndarray[numpy.float64_t, ndim = 1] edges0, edges1
+        numpy.ndarray[numpy.float64_t, ndim = 2] out_data = numpy.zeros((bins0, bins1), dtype="float64")
+        numpy.ndarray[numpy.float64_t, ndim = 2] out_count = numpy.zeros((bins0, bins1), dtype="float64")
+        numpy.ndarray[numpy.float64_t, ndim = 2] out_merge = numpy.zeros((bins0, bins1), dtype="float64")
         double min0 = pos0.min()
         double max0 = pos0.max() * EPS32
         double min1 = pos1.min()
         double max1 = pos1.max() * EPS32
-        double delta0 = (max0 - min0) / float(bin0) 
-        double delta1 = (max1 - min1) / float(bin1)  
-        double dbin0 = 0.5, dbin1 = 0.5
-        double fbin0, fbin1, p0, p1, d, rest, delta0l, delta0r, delta1l, delta1r, aera
+        double delta0 = (max0 - min0) / float(bins0)
+        double delta1 = (max1 - min1) / float(bins1)
+        double fbin0, fbin1, p0, p1, d
         double epsilon = 1e-10
 
-    edges0 = numpy.linspace(min0 + (0.5 * delta0), max0 - (0.5 * delta0), bin0)
-    edges1 = numpy.linspace(min1 + (0.5 * delta1), max1 - (0.5 * delta1), bin1)
+    if split:
+        logger.warning("No pixel splitting in histogram")
+
+    edges0 = numpy.linspace(min0 + (0.5 * delta0), max0 - (0.5 * delta0), bins0)
+    edges1 = numpy.linspace(min1 + (0.5 * delta1), max1 - (0.5 * delta1), bins1)
     if nthread is not None:
         if isinstance(nthread, int) and (nthread > 0):
             omp_set_num_threads(< int > nthread)
@@ -254,94 +217,15 @@ def histogram2d(numpy.ndarray pos0 not None,
             d = data[i]
             fbin0 = getBinNr(p0, min0, delta0)
             fbin1 = getBinNr(p1, min1, delta1)
-            b0 = < int > floor(fbin0)
-            b1 = < int > floor(fbin1)
-            if b0 == bin0:
-                b0 = bin0 - 1
-                fbin0 = (< double > bin0) - 0.5
-            elif b0 == 0:
-                fbin0 = 0.5
-            if b1 == bin1:
-                b1 = bin1 - 1
-                fbin1 = (< double > bin1) - 0.5
-            elif b1 == 0:
-                fbin1 = 0.5
+            bin0 = < int > floor(fbin0)
+            bin1 = < int > floor(fbin1)
+            if (bin0<0) or (bin1<0) or (bin0>=bins0) or (bin1>=bins1):
+                continue
+            out_count[bin0, bin1] += 1.0
+            out_data[bin0, bin1] += d
 
-            rest = 1.0
-            if csplit == 1:
-                delta0l = fbin0 - < double > b0 - dbin0
-                delta0r = fbin0 - < double > b0 - 1 + dbin0
-                delta1l = fbin1 - < double > b1 - dbin1
-                delta1r = fbin1 - < double > b1 - 1 + dbin1                
-                if delta0l < 0 and b0 > 0:
-                    if delta1l < 0 and b1 > 0:
-                        area = delta0l * delta1l
-                        rest -= area
-                        out_count[b0 - 1, b1 - 1] += area
-                        out_data[b0 - 1, b1 - 1] += area * d
-
-                        area = (-delta0l) * (1 + delta1l)
-                        rest -= area
-                        out_count[b0 - 1, b1] += area
-                        out_data[b0 - 1, b1] += area * d
-
-                        area = (1 + delta0l) * (-delta1l)
-                        rest -= area
-                        out_count[b0, b1 - 1] += area
-                        out_data[b0, b1 - 1] += area * d
-
-                    elif delta1r > 0 and b1 < bin1 - 1:
-                        area = -delta0l * delta1r
-                        rest -= area
-                        out_count[b0 - 1, b1 + 1] += area
-                        out_data[b0 - 1, b1 + 1] += area * d
-
-                        area = (-delta0l) * (1 - delta1r)
-                        rest -= area
-                        out_count[b0 - 1, b1] += area
-                        out_data[b0 - 1, b1] += area * d
-
-                        area = (1 + delta0l) * (delta1r)
-                        rest -= area
-                        out_count[b0, b1 + 1] += area
-                        out_data[b0, b1 + 1] += area * d
-                elif delta0r > 0 and b0 < bin0 - 1:
-                    if delta1l < 0 and b1 > 0:
-                        area = -delta0r * delta1l
-                        rest -= area
-                        out_count[b0 + 1, b1 - 1] += area
-                        out_data[b0 + 1, b1 - 1] += area * d
-
-                        area = (delta0r) * (1 + delta1l)
-                        rest -= area
-                        out_count[b0 + 1, b1] += area
-                        out_data[b0 + 1, b1] += area * d
-
-                        area = (1 - delta0r) * (-delta1l)
-                        rest -= area
-                        out_count[b0, b1 - 1] += area
-                        out_data[b0, b1 - 1] += area * d
-
-                    elif delta1r > 0 and b1 < bin1 - 1:
-                        area = delta0r * delta1r
-                        rest -= area
-                        out_count[b0 + 1, b1 + 1] += area
-                        out_data[b0 + 1, b1 + 1] += area * d
-
-                        area = (delta0r) * (1 - delta1r)
-                        rest -= area
-                        out_count[b0 + 1, b1] += area
-                        out_data[b0 + 1, b1] += area * d
-
-                        area = (1 - delta0r) * (delta1r)
-                        rest -= area
-                        out_count[b0, b1 + 1] += area
-                        out_data[b0, b1 + 1] += area * d
-            out_count[b0, b1] += rest
-            out_data[b0, b1] += d * rest
-
-        for i in prange(bin0):
-            for j in range(bin1):
+        for i in prange(bins0):
+            for j in range(bins1):
                 if out_count[i, j] > epsilon:
                     out_merge[i, j] += out_data[i, j] / out_count[i, j]
                 else:
