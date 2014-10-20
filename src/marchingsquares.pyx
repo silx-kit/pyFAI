fb874cfe0d667f71d5ee9c6a1ae88ea9b7185e1e
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-10-20 14:07:42 +0200

    Marching squares is under BSD (new). Agreed with Almar Klein

--- src/marchingsquares.pyx
+++ src/marchingsquares.pyx
@@ -1,6 +1,30 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2012, Almar Klein
-# Copyright (C) 2014, Jerome Kieffer
+# Copyright (C) 2014, Jerome Kieffer (European Synchrotron Radiation Facility)
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 # Cython specific imports
 
 import numpy 
@@ -11,59 +35,59 @@ cdef double epsilon = numpy.finfo(numpy.float64).eps
 from cython.view cimport array as cvarray
 from .utils import timeit
 
-cdef numpy.int8_t[:,:] EDGETORELATIVEPOSX = numpy.array([ [0, 1], [1, 1], [1, 0], [0, 0], [0, 1], [1, 1], [1, 0], [0, 0], [0, 0], [1, 1], [1, 1], [0, 0] ], dtype=numpy.int8)
-cdef numpy.int8_t[:,:] EDGETORELATIVEPOSY = numpy.array([ [0, 0], [0, 1], [1, 1], [1, 0], [0, 0], [0, 1], [1, 1], [1, 0], [0, 0], [0, 0], [1, 1], [1, 1] ], dtype=numpy.int8)
-cdef numpy.int8_t[:,:] EDGETORELATIVEPOSZ = numpy.array([ [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [1, 1], [1, 1], [1, 1], [0, 1], [0, 1], [0, 1], [0, 1] ], dtype=numpy.int8)
-cdef numpy.int8_t[:,:] CELLTOEDGE = numpy.array([
-                                                    [0, 0, 0, 0, 0], # Case 0: nothing
-                                                    [1, 0, 3, 0, 0], # Case 1
-                                                    [1, 0, 1, 0, 0], # Case 2
-                                                    [1, 1, 3, 0, 0], # Case 3
+cdef numpy.int8_t[:, :] EDGETORELATIVEPOSX = numpy.array([[0, 1], [1, 1], [1, 0], [0, 0], [0, 1], [1, 1], [1, 0], [0, 0], [0, 0], [1, 1], [1, 1], [0, 0]], dtype=numpy.int8)
+cdef numpy.int8_t[:, :] EDGETORELATIVEPOSY = numpy.array([[0, 0], [0, 1], [1, 1], [1, 0], [0, 0], [0, 1], [1, 1], [1, 0], [0, 0], [0, 0], [1, 1], [1, 1]], dtype=numpy.int8)
+cdef numpy.int8_t[:, :] EDGETORELATIVEPOSZ = numpy.array([[0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [1, 1], [1, 1], [1, 1], [0, 1], [0, 1], [0, 1], [0, 1]], dtype=numpy.int8)
+cdef numpy.int8_t[:, :] CELLTOEDGE = numpy.array([
+                                                    [0, 0, 0, 0, 0],  # Case 0: nothing
+                                                    [1, 0, 3, 0, 0],  # Case 1
+                                                    [1, 0, 1, 0, 0],  # Case 2
+                                                    [1, 1, 3, 0, 0],  # Case 3
+                             
+                                                    [1, 1, 2, 0, 0],  # Case 4
+                                                    [2, 0, 1, 2, 3],  # Case 5 > ambiguous
+                                                    [1, 0, 2, 0, 0],  # Case 6
+                                                    [1, 2, 3, 0, 0],  # Case 7
+                             
+                                                    [1, 2, 3, 0, 0],  # Case 8
+                                                    [1, 0, 2, 0, 0],  # Case 9
+                                                    [2, 0, 3, 1, 2],  # Case 10 > ambiguous
+                                                    [1, 1, 2, 0, 0],  # Case 11
                             
-                                                    [1, 1, 2, 0, 0], # Case 4
-                                                    [2, 0, 1, 2, 3], # Case 5 > ambiguous
-                                                    [1, 0, 2, 0, 0], # Case 6
-                                                    [1, 2, 3, 0, 0], # Case 7
-                            
-                                                    [1, 2, 3, 0, 0], # Case 8
-                                                    [1, 0, 2, 0, 0], # Case 9
-                                                    [2, 0, 3, 1, 2], # Case 10 > ambiguous
-                                                    [1, 1, 2, 0, 0], # Case 11
-                            
-                                                    [1, 1, 3, 0, 0], # Case 12
-                                                    [1, 0, 1, 0, 0], # Case 13
-                                                    [1, 0, 3, 0, 0], # Case 14
-                                                    [0, 0, 0, 0, 0], # Case 15
-                                                    ], dtype=numpy.int8)
+                                                    [1, 1, 3, 0, 0],  # Case 12
+                                                    [1, 0, 1, 0, 0],  # Case 13
+                                                    [1, 0, 3, 0, 0],  # Case 14
+                                                    [0, 0, 0, 0, 0],  # Case 15
+                                                ], dtype=numpy.int8)
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-def marching_squares(float[:,:] img, double isovalue,
-                     numpy.int8_t[:,:] cellToEdge, 
-                     numpy.int8_t[:,:] edgeToRelativePosX, 
-                     numpy.int8_t[:,:] edgeToRelativePosY):
-    cdef int dim_y = img.shape[0]
-    cdef int dim_x = img.shape[1]
-    #output arrays
-    cdef numpy.ndarray[numpy.float32_t, ndim=2] edges = numpy.zeros((dim_x*dim_y,2), numpy.float32)
-
-    cdef int x, y, z, i, j, k, index,edgeCount = 0
-    cdef int dx1, dy1, dz1, dx2, dy2, dz2
-    cdef double fx, fy, fz, ff, tmpf, tmpf1, tmpf2
+def marching_squares(float[:, :] img, double isovalue,
+                     numpy.int8_t[:, :] cellToEdge, 
+                     numpy.int8_t[:, :] edgeToRelativePosX, 
+                     numpy.int8_t[:, :] edgeToRelativePosY):
+    cdef:
+        int dim_y = img.shape[0]
+        int dim_x = img.shape[1]
+        numpy.ndarray[numpy.float32_t, ndim = 2] edges = numpy.zeros((dim_x * dim_y, 2), numpy.float32)
+        int x, y, z, i, j, k, index, edgeCount = 0
+        int dx1, dy1, dz1, dx2, dy2, dz2
+        double fx, fy, fz, ff, tmpf, tmpf1, tmpf2
     with nogil:
-        for y in range(dim_y-1):
-            for x in range(dim_x-1):
+        for y in range(dim_y - 1):
+            for x in range(dim_x - 1):
     
                 # Calculate index.
                 index = 0
                 if img[y, x] > isovalue:
                     index += 1
-                if img[y, x+1] > isovalue:
+                if img[y, x + 1] > isovalue:
                     index += 2
-                if img[y+1, x+1] > isovalue:
+                if img[y + 1, x + 1] > isovalue:
                     index += 4
-                if img[y+1, x] > isovalue:
+                if img[y + 1, x] > isovalue:
                     index += 8
     
                 # Resolve ambiguity
@@ -91,62 +115,64 @@ def marching_squares(float[:,:] img, double isovalue,
                         tmpf2 = 1.0 / (epsilon + fabs( img[y+dy2,x+dx2] - isovalue))
                         # Apply a kind of center-of-mass method
                         fx, fy, ff = 0.0, 0.0, 0.0
-                        fx += <double>dx1 * tmpf1;  fy += <double>dy1 * tmpf1;  ff += tmpf1
-                        fx += <double>dx2 * tmpf2;  fy += <double>dy2 * tmpf2;  ff += tmpf2
+                        fx += <double> dx1 * tmpf1; fy += <double>dy1 * tmpf1;  ff += tmpf1
+                        fx += <double> dx2 * tmpf2; fy += <double>dy2 * tmpf2;  ff += tmpf2
                         #
                         fx /= ff
                         fy /= ff
                         # Append point
-                        edges[edgeCount,0] = <float>(x + fx)
-                        edges[edgeCount,1] = <float>(y + fy)
+                        edges[edgeCount,0] = <float> (x + fx)
+                        edges[edgeCount,1] = <float> (y + fy)
                         edgeCount += 1
 
     # Done
-    return edges[:edgeCount,:]
+    return edges[:edgeCount, : ]
+
 
 @cython.boundscheck(False)
 def sort_edges(edges):
     """
     Reorder edges in such a way they become contiguous
     """
-    cdef int size =  edges.shape[0]
-    cdef int[:] pos = cvarray(shape=(size,), itemsize=sizeof(int), format="i")
-    cdef int[:] remaining = cvarray(shape=(size,), itemsize=sizeof(int), format="i")
-    cdef float[:,:] dist2 = cvarray(shape=(size,size), itemsize=sizeof(float), format="f")
-    dist2[:,:] = 0
+    cdef:
+        int size = edges.shape[0]
+        int[:] pos = cvarray(shape=(size,), itemsize=sizeof(int), format="i")
+        int[:] remaining = cvarray(shape=(size,), itemsize=sizeof(int), format="i")
+        float[:, :] dist2 = cvarray(shape=(size, size), itemsize=sizeof(float), format="f")
+        float d
+        int i, j, index = 0, current = 0
+        float[:, :]edges_ = numpy.ascontiguousarray(edges, numpy.float32)      
+    dist2[:, :] = 0
     pos[:] = 0
-    cdef float d
-    cdef int i,j, index=0, current=0
-    cdef float[:,:]edges_ = numpy.ascontiguousarray(edges, numpy.float32)      
     
     with nogil:
-        #initialize the distance (squared) array:
+        # initialize the distance (squared) array:
         for i in range(size):
             remaining[i] = i
-            for j in range(i+1,size):
-                d = (edges_[i,0]-edges_[j,0])**2 + (edges_[i,1]-edges_[j,1])**2 
+            for j in range(i + 1, size):
+                d = (edges_[i, 0] - edges_[j, 0]) ** 2 + (edges_[i, 1] - edges_[j, 1]) ** 2 
                 dist2[i, j] = d
                 dist2[j, i] = d
-        #set element in remaining to -1 when already transfered
+        # set element in remaining to -1 when already transfered
         # O(n^2) implementation, not bright, any better idea is welcome
         remaining[0] = -1
         pos[0] = 0
         for i in range(1, size):
-            current = pos[i-1]
+            current = pos[i - 1]
             index = -1
-            for j in range(1,size):
+            for j in range(1, size):
                 if remaining[j] == -1:
                     continue
-                elif index==-1: #not yet found a candidate
-                    index=remaining[j]
-                    d = dist2[index,current]
+                elif index == -1:  # not yet found a candidate
+                    index = remaining[j]
+                    d = dist2[index, current]
                     continue
-                elif dist2[j,current]<d:    
+                elif dist2[j, current] < d:    
                     index = j
-                    d = dist2[current,index]
+                    d = dist2[current, index]
             pos[i] = index
             remaining[index] = -1
-    return edges[pos,:]
+    return edges[pos, :]
 
 
 def isocontour(img, isovalue=None, sorted=False):
@@ -175,8 +201,8 @@ def isocontour(img, isovalue=None, sorted=False):
     else:
         # Will raise error if not float-like value given
         isovalue = float(isovalue)
-    res =  marching_squares(numpy.ascontiguousarray(img, numpy.float32),  isovalue,
-                            CELLTOEDGE, EDGETORELATIVEPOSX,EDGETORELATIVEPOSY)
+    res = marching_squares(numpy.ascontiguousarray(img, numpy.float32), isovalue,
+                           CELLTOEDGE, EDGETORELATIVEPOSX, EDGETORELATIVEPOSY)
     if sorted:
         return sort_edges(res)
     else:
