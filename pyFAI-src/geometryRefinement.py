873f80448a11794979da7be1d24e108e5b7cd1de
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-11-07 17:33:04 +0100

    still related to #154

--- pyFAI-src/geometryRefinement.py
+++ pyFAI-src/geometryRefinement.py
@@ -84,7 +84,7 @@ class GeometryRefinement(AzimuthalIntegrator):
         self.data = numpy.array(data, dtype=numpy.float64)
         assert self.data.ndim == 2
         assert self.data.shape[1] in [ 3, 4] #3 for non weighted, 4 for weighted refinement
-        assert self.data.shape[0]>0
+        assert self.data.shape[0] > 0
 
         if (pixel1 is None) and (pixel2 is None) and (splineFile is None) and (detector is None):
             raise RuntimeError("Setting up the geometry refinement without knowing the detector makes little sense")
@@ -432,35 +432,49 @@ class GeometryRefinement(AzimuthalIntegrator):
         return self.residu2_wavelength(param,
                             self.data[:, 0], self.data[:, 1], self.data[:, 2])
 
-    def curve_fit(self):
+    def curve_fit(self, with_rot=True):
         """Refine the geometry and provide confidence interval
         Use curve_fit from scipy.optimize to not only refine the geometry (unconstrained fit)
 
+        @param with_rot: include rotation intro error measurment
         @return errors
         """
         d1 = self.data[:, 0]
         d2 = self.data[:, 1]
+        size = d1.size
         x = d1, d2
         rings = self.data[:, 2].astype(numpy.int32)
-        f = lambda x, *param: self.tth(x[0], x[1], numpy.concatenate(param, [0]))
+        f_with_rot = lambda x, *param: self.tth(x[0], x[1], numpy.concatenate((param, [self.rot3])))
+        f_no_rot = lambda x, *param: self.tth(x[0], x[1], numpy.concatenate((param, [self.rot1, self.rot2, self.rot3])))
         y = self.calc_2th(rings, self.wavelength)
         param0 = numpy.array([self.dist, self.poni1, self.poni2, self.rot1, self.rot2, self.rot3], dtype=numpy.float64)
         ref = self.residu2(param0, d1, d2, rings)
         print("param0: %s %s" % (param0, ref))
-        popt, pcov = curve_fit(f, x, y, param0[:-1])
-        popt = numpy.concatenate(param, self.rot3)
+        if with_rot:
+            popt, pcov = curve_fit(f, x, y, param0[:-1])
+            popt = numpy.concatenate((popt, [self.rot3]))
+        else:
+            popt, pcov = curve_fit(f, x, y, param0[:-1])
+            popt = numpy.concatenate((popt, [self.rot1, self.rot2, self.rot3]))
         obt = self.residu2(popt, d1, d2, rings)
         print("param1: %s %s" % (popt, obt))
         print(pcov)
         err = numpy.sqrt(numpy.diag(pcov))
         print("err: %s" % err)
-        if obt<ref:
+        if obt < ref:
             self.param = popt
             self.dist, self.poni1, self.poni2, \
                     self.rot1, self.rot2, self.rot3 = tuple(popt)
-        return err
+        error = {}
+        confidence = {}
+        for k, v in zip(("dist", "poni1", "poni2", "rot1", "rot2", "rot3"), err):
+            error[k] = v
+            confidence[k] = 1.96 * err / numpy.sqrt(size)
 
-    @timeit
+        print("Error as sqrt of the diag of covariance:\n%s" % error)
+        print("Confidence as 1.95 sigma/sqrt(n):\n%s" % confidence)
+        return error, confidence
+
     def confidence(self, with_rot=True):
         """Confidence interval obtained from the inverse of the hessian matrix
         of the error function next to its minimum value.
@@ -479,7 +493,7 @@ class GeometryRefinement(AzimuthalIntegrator):
             size = 5
         else:
             size = 3
-        hessian = numpy.zeros((size,size), dtype=numpy.float64)
+        hessian = numpy.zeros((size, size), dtype=numpy.float64)
 
         delta = abs(epsilon * param0)
         delta[abs(param0) < epsilon] = epsilon
@@ -523,7 +537,16 @@ class GeometryRefinement(AzimuthalIntegrator):
         print(cov)
         err = numpy.sqrt(numpy.diag(cov))
         print("err: %s" % err)
-        return err
+        error = {}
+        for k, v in zip(("dist", "poni1", "poni2", "rot1", "rot2", "rot3"), err):
+            error[k] = v
+        confidence = {}
+        for i, k in enumerate(("dist", "poni1", "poni2", "rot1", "rot2", "rot3")):
+            if i < size:
+                confidence[k] = numpy.sqrt(ref / hessian[i, i])
+        print("Error as sqrt of the diag of inv hessian:\n%s" % error)
+        print("Convidence as sqrt of the error function /  hessian:\n%s" % confidence)
+        return error, confidence
 
     def roca(self):
         """
