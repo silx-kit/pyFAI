4643c20c208867092142983ded8603da40bdddc1
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-03-16 16:05:01 +0100

    add header & clean up

--- scripts/eiger_mask.py
+++ scripts/eiger_mask.py
@@ -1,12 +1,50 @@
 #!/usr/bin/python
 # coding: UTF-8
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+#
+#    Project: Azimuthal integration
+#             https://github.com/pyFAI/pyFAI
+#
+#    Copyright (C) European Synchrotron Radiation Facility, Grenoble, France
+#
+#    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+eiger_mask
+
+A tool for extracting the mask from an Eiger master file.
+
+$ eiger_mask master.h5 
+"""
+
+__author__ = "Jerome Kieffer"
+__contact__ = "Jerome.Kieffer@ESRF.eu"
+__license__ = "GPLv3+"
+__copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+__date__ = "16/03/2015"
+__satus__ = "development"
+
 
 import os
 import sys
 import h5py
 import numpy
 import fabio
+
 
 def extract_mask(infile):
     """
@@ -23,15 +61,15 @@ def extract_mask(infile):
 
 if __name__ == "__main__":
     infile = os.path.abspath(sys.argv[1])
-    if len(sys.argv)>2:
+    if len(sys.argv) > 2:
         outfile = sys.argv[2]
     else:
-        outfile = os.path.splitext(infile)[0]+"_mask.edf"
+        outfile = os.path.splitext(infile)[0] + "_mask.edf"
     mask = extract_mask(infile)
     if outfile.endswith("msk"):
         fabio.fit2dmaskimage.fit2dmaskimage(data=mask).write(outfile)
     elif outfile.endswith("tif"):
         fabio.tifimage.tifimage(data=mask).write(outfile)
     else:
-        fabio.edfimage.edfimage(header={"data_file":infile},data=mask).write(outfile)
+        fabio.edfimage.edfimage(header={"data_file": infile}, data=mask).write(outfile)
 
