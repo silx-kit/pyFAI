1c114af6f500ac74669a1f4c003d21292cbbc973
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-06-30 14:35:55 +0200

    compatibility with python3

--- update_openmp.sh
+++ update_openmp.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+if [ $1 = 3 ];
+then CYTHON=cython3
+else CYTHON=cython
+fi
+
 rm -f src/histogram.c
 rm -f src/histogram.so
 rm -f src/histogram.pyx
 echo "OpenMP code"
 cp src/histogram_omp.pyx src/histogram.pyx
-cython -a src/histogram.pyx --fast-fail
+$CYTHON -a src/histogram.pyx --fast-fail
 cp src/histogram.c src/histogram_omp.c
 cp src/histogram.html src/histogram_omp.html
 
@@ -13,7 +18,7 @@ rm -f src/histogram.so
 rm -f src/histogram.pyx
 echo "No OpenMP code"
 cp src/histogram_nomp.pyx src/histogram.pyx
-cython -a src/histogram.pyx --fast-fail
+$CYTHON -a src/histogram.pyx --fast-fail
 cp src/histogram.c src/histogram_nomp.c
 cp src/histogram.html src/histogram_nomp.html
 
