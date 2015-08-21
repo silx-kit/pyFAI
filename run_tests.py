47c7e6426a1901fba32e6c8eb88e30adb893eb6f
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-08-21 14:47:04 +0200

    typo

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys, numpy, scipy, fabio, pyFAI
 print("Python %s %s" % (sys.version, tuple.__itemsize__ * 8))
-print("Numpy %s", numpy.version.version)
+print("Numpy %s" % numpy.version.version)
 print("Scipy %s" % scipy.version.version)
 print("FabIO %s" % fabio.version)
 print("PyFAI %s" % pyFAI.version)
