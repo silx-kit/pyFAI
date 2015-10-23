8dad93917d2930acdc382e49f5e8ab6f9c4463ce
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-10-23 09:03:22 +0200

    declare two files for diffraction tomography experiments

---
+++ pyFAI-src/diffmap_widget.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+#
+#    Project: Azimuthal integration
+#             https://github.com/pyFAI/pyFAI
+#
+#    Copyright (C) 2015 European Synchrotron Radiation Facility, Grenoble, France
+#
+#    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+from __future__ import absolute_import, print_function, division
+
+__author__ = "Jerome Kieffer"
+__contact__ = "Jerome.Kieffer@ESRF.eu"
+__license__ = "MIT"
+__copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+__date__ = "22/10/2015"
+__status__ = "development"
+__docformat__ = 'restructuredtext'
+__doc__ = """
+
+Module with GUI for diffraction mapping experiments 
+
+
+"""
+# __all__ = ["date", "version_info", "strictversion", "hexversion"]
+
+from .gui_utils import QtGui, QtCore, QtUiTools, uic
+from .utils import float_, int_, str_, get_ui_file
+
+uif = "diffmap.ui"
+
+class DiffMapWidget(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        try:
+            uic.loadUi(get_ui_file(uif), self)
+        except AttributeError as error:
+            logger.error("I looks like your installation suffers from this bug: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=697348")
+            raise RuntimeError("Please upgrade your installation of PyQt (or apply the patch)")
