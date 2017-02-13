74ab72013308f3c3e59126f884992fc2847cc34e
Valentin Valls <valentin.valls@esrf.fr>
2017-02-13 18:40:16 +0100

    Create a basic calibration GUI
    - It only contains a working calibrant selection

---
+++ pyFAI/gui/calibration/ExperimentTask.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+# /*##########################################################################
+#
+# Copyright (c) 2016 European Synchrotron Radiation Facility
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ###########################################################################*/
+
+__authors__ = ["V. Valls"]
+__license__ = "MIT"
+__date__ = "13/02/2017"
+
+from __future__ import absolute_import
+
+from pyFAI.gui import qt
+import pyFAI.utils
+from pyFAI.gui.calibration.AbstractCalibrationTask import AbstractCalibrationTask
+
+
+class ExperimentTask(AbstractCalibrationTask):
+
+    def __init__(self):
+        super(ExperimentTask, self).__init__()
+        qt.loadUi(pyFAI.utils.get_ui_file("calibration-experiment.ui"), self)
+
+    def _updateModel(self, model):
+        self._calibrant.setModel(model.experimentSettingsModel().calibrantModel())
+        model.experimentSettingsModel().calibrantModel().changed.connect(self.printSelectedCalibrant)
+
+    def printSelectedCalibrant(self):
+        print(self.model().experimentSettingsModel().calibrantModel().calibrant())
