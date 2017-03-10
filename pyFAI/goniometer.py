984f9b4acfa70a964ed49cff5e7ce381006547a9
Jerome Kieffer <jerome.kieffer@esrf.fr>
2017-03-10 16:50:16 +0100

    Cope for empty images

--- pyFAI/goniometer.py
+++ pyFAI/goniometer.py
@@ -493,7 +493,7 @@ class GoniometerRefinement(Goniometer):
         for single in self.single_geometries.values():
             motor_pos = single.get_position()
             single_param = self.translation_function(param, motor_pos)
-            if single.geometry_refinement is not None:
+            if single.geometry_refinement is not None and len(single.geometry_refinement.data) > 1:
                 sumsquare += single.geometry_refinement.chi2(single_param)
                 npt += single.geometry_refinement.data.shape[0]
         return sumsquare / max(npt, 1)
