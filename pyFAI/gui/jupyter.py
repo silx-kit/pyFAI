1aaa4a989b030183a7319bdbbb6a7ecfdd9c3b5a
Jerome Kieffer <jerome.kieffer@esrf.fr>
2017-03-24 17:25:16 +0100

    move jupyter to gui

--- pyFAI/gui/jupyter.py
+++ pyFAI/gui/jupyter.py
@@ -26,7 +26,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-"""Jupyter helper function
+"""Jupyter helper functions
 """
 
 from __future__ import division, print_function
