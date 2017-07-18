03f76fb4f8f7e7ff1cdab950a993e73589ae359f
Valentin Valls <valentin.valls@esrf.fr>
2017-07-18 19:38:26 +0200

    Add few tests for pyFAI-integrate

--- pyFAI/test/test_all.py
+++ pyFAI/test/test_all.py
@@ -32,7 +32,7 @@ __authors__ = ["Jérôme Kieffer"]
 __contact__ = "jerome.kieffer@esrf.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "09/03/2017"
+__date__ = "18/07/2017"
 
 import sys
 import unittest
@@ -80,6 +80,7 @@ from . import test_preproc
 from . import test_bayes
 from . import test_scripts
 from . import test_goniometer
+from . import test_integrate_app
 from ..opencl import test as test_opencl
 
 
@@ -97,6 +98,7 @@ def suite():
     testsuite.addTest(test_export.suite())
     testsuite.addTest(test_saxs.suite())
     testsuite.addTest(test_integrate.suite())
+    testsuite.addTest(test_integrate_app.suite())
     testsuite.addTest(test_bilinear.suite())
     testsuite.addTest(test_distortion.suite())
     testsuite.addTest(test_flat.suite())
