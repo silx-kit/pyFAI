f4c95f727f9847321046560ff37147a3acd8fe34
Jerome Kieffer <jerome.kieffer@esrf.fr>
2017-09-18 14:25:18 +0200

    increment version

--- version.py
+++ version.py
@@ -62,10 +62,10 @@ RELEASE_LEVEL_VALUE = {"dev": 0,
                        "final": 15}
 
 MAJOR = 0
-MINOR = 14
-MICRO = 2
-RELEV = "final"  # <16
-SERIAL = 0  # <16
+MINOR = 15
+MICRO = 0
+RELEV = "dev"  # <16
+SERIAL = 1  # <16
 
 date = __date__
 
