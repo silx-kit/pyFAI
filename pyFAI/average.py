31a02c74192a13268a0e8ba83178c0f3d553ba59
Jerome Kieffer <kieffer@lintaillefer.esrf.fr>
2017-10-30 11:03:29 +0100

    Atempt to fixe 719 (only for single framed files)

--- pyFAI/average.py
+++ pyFAI/average.py
@@ -797,19 +797,20 @@ class Average(object):
         """
         self._fabio_images = []
         self._nb_frames = 0
-        if len(image_list) > 1000:
-            # if too many files are opened, it may crash
+        if len(image_list) > 100:
+            # if too many files are opened, it may crash. The har limit is 1024
             copy_data = True
         else:
             copy_data = False
         for image_index, image in enumerate(image_list):
             if isinstance(image, six.string_types):
                 logger.info("Reading %s", image)
-                if copy_data:
-                    with fabio.open(image) as fimg:
-                        fabio_image = fimg.convert(fimg.__class__)
-                else:
-                    fabio_image = fabio.open(image)
+                fabio_image = fabio.open(image)
+                if copy_data and fabio_image.nframes == 1:
+                    # copy the data so that we can close the file
+                    fimg = fabio_image.convert(fabio_image.__class__)
+                    fabio_image.close()
+                    fabio_image = fimg
             elif isinstance(image, fabio.fabioimage.fabioimage):
                 fabio_image = image
             else:
