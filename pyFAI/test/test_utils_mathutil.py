9d5201931cac608302b66461b8e9eeef3280fef7
Valentin Valls <valentin.valls@esrf.fr>
2018-01-11 15:15:01 +0100

    Test round_fft

--- pyFAI/test/test_utils_mathutil.py
+++ pyFAI/test/test_utils_mathutil.py
@@ -41,14 +41,45 @@ import unittest
 import numpy
 import os
 import logging
-from .utilstest import UtilsTest
+from . import utilstest
 logger = logging.getLogger(__name__)
 from .. import utils
 
 import scipy.ndimage
 
 
-class TestMathUtil(unittest.TestCase):
+_ROUND_FFT_VALUES = [
+    (2, 2), (3, 3), (5, 5), (7, 7), (11, 11), (13, 13), (17, 18), (19, 20),
+    (23, 24), (29, 30), (31, 32), (37, 39), (41, 42), (43, 44), (47, 48),
+    (53, 54), (59, 60), (61, 63), (67, 70), (71, 72), (73, 75), (79, 80),
+    (83, 84), (89, 90), (97, 98), (101, 104), (103, 104), (107, 108),
+    (109, 110), (113, 117), (127, 128), (131, 132), (137, 140), (139, 140),
+    (149, 150), (151, 154), (157, 160), (163, 165), (167, 168), (173, 175),
+    (179, 180), (181, 182), (191, 192), (193, 195), (197, 198), (199, 200),
+    (211, 216), (223, 224), (227, 231), (229, 231), (233, 234), (239, 240),
+    (241, 243), (251, 252), (257, 260), (263, 264), (269, 270), (271, 273),
+    (277, 280), (281, 288), (283, 288), (293, 294), (307, 308), (311, 312),
+    (313, 315), (317, 320), (331, 336), (337, 343), (347, 350), (349, 350),
+    (353, 360), (359, 360), (367, 375), (373, 375), (379, 384), (383, 384),
+    (389, 390), (397, 400), (401, 405), (409, 416), (419, 420), (421, 432),
+    (431, 432), (433, 440), (439, 440), (443, 448), (449, 450), (457, 462),
+    (461, 462), (463, 468), (467, 468), (479, 480), (487, 490), (491, 495),
+    (499, 500), (503, 504), (509, 512), (521, 525), (523, 525), (541, 546),
+    (547, 550), (557, 560), (563, 567), (569, 576), (571, 576), (577, 585),
+    (587, 588), (593, 594), (599, 600), (601, 616), (607, 616), (613, 616),
+    (617, 624), (619, 624), (631, 637), (641, 648), (643, 648), (647, 648),
+    (653, 660), (659, 660), (661, 672), (673, 675), (677, 686), (683, 686),
+    (691, 693), (701, 702), (709, 720), (719, 720), (727, 728), (733, 735),
+    (739, 750), (743, 750), (751, 756), (757, 768), (761, 768), (769, 770),
+    (773, 780), (787, 792), (797, 800), (809, 810), (811, 819), (821, 825),
+    (823, 825), (827, 832), (829, 832), (839, 840), (853, 864), (857, 864),
+    (859, 864), (863, 864), (877, 880), (881, 882), (883, 891), (887, 891),
+    (907, 910), (911, 924), (919, 924), (929, 936), (937, 945), (941, 945),
+    (947, 960), (953, 960), (967, 972), (971, 972), (977, 980), (983, 990),
+    (991, 1000), (997, 1000)]
+
+
+class TestMathUtil(utilstest.ParametricTestCase):
 
     def setUp(self):
         unittest.TestCase.setUp(self)
@@ -56,11 +87,17 @@ class TestMathUtil(unittest.TestCase):
         self.dark = self.unbinned.astype("float32")
         self.flat = 1 + numpy.random.random((64, 32))
         self.raw = self.flat + self.dark
-        self.tmp_file = os.path.join(UtilsTest.tempdir, "testUtils_average.edf")
+        self.tmp_file = os.path.join(utilstest.UtilsTest.tempdir, "testUtils_average.edf")
 
     def tearDown(self):
         unittest.TestCase.tearDown(self)
         self.dark = self.flat = self.raw = self.tmp_file = None
+
+    def test_round_fft(self):
+        """Test some rounding values."""
+        for value, expected in _ROUND_FFT_VALUES:
+            with self.subTest(value=value, expected=expected):
+                self.assertEqual(utils.round_fft(value), expected)
 
     def test_binning(self):
         """
@@ -114,4 +151,4 @@ def suite():
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
     runner.run(suite())
-    UtilsTest.clean_up()
+    utilstest.UtilsTest.clean_up()
