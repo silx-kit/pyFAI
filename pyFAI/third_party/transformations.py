cd0c438ef207c07cacd4220e8b97656871de37ef
Jerome Kieffer <jerome.kieffer@esrf.fr>
2018-04-16 09:24:34 +0200

    provide the transformation library from C. Gohlke.
    This is not known to be packaged elsewhere. The license is compatible
    (BSD)

---
+++ pyFAI/third_party/transformations.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+# /*##########################################################################
+#
+# Copyright (C) 2015-2018 European Synchrotron Radiation Facility
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ###########################################################################*/
+"""Wrapper module for the `transformations` library.
+
+Feed this module using a local pyFAI copy of `transformations` if it exists.
+Else it expect to have an available `transformations` library installed in the 
+Python path.
+
+It should be used like that:
+
+.. code-block::
+
+    from pyFAI.third_party import transformations
+
+"""
+
+from __future__ import absolute_import
+
+__authors__ = ["Valentin Valls"]
+__license__ = "MIT"
+__date__ = "16/04/2018"
+
+try:
+    # try to import our local version of six
+    from ._local.transformations import *  # noqa
+except ImportError:
+    # else try to import it from the python path
+    from transformations import *  # noqa
