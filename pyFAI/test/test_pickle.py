b7c6db574d0f6995f5c91dd1474360df8905b1c4
Jerome Kieffer <jerome.kieffer@esrf.fr>
2018-06-27 16:57:31 +0200

    Improve the tests (less memory, faster, ...)

--- pyFAI/test/test_pickle.py
+++ pyFAI/test/test_pickle.py
@@ -1,50 +1,67 @@
 
-import pyFAI
-from pyFAI import AzimuthalIntegrator
-from pyFAI.detectors import Pilatus1M, Detector
+import numpy
+from pyFAI.azimuthalIntegrator import AzimuthalIntegrator
+from pyFAI.geometry import Geometry
+from pyFAI.detectors import detector_factory
 from pickle import dumps, loads
 import unittest
-from .utilstest import UtilsTest
-import fabio
 import logging
 logger = logging.getLogger(__name__)
 
 
 class TestPickle(unittest.TestCase):
 
-    def setUp(self):
-        unittest.TestCase.setUp(self)
-        img = UtilsTest.getimage("Pilatus1M.edf")
-        self.data = fabio.open(img).data
-        self.ai = AzimuthalIntegrator(1.58323111834, 0.0334170169115, 0.0412277798782, 0.00648735642526, 0.00755810191106, 0.0, detector=Pilatus1M())
-        self.ai.wavelength = 1e-10
-        self.npt = 1000
+    @classmethod
+    def setUpClass(cls):
+        super(TestPickle, cls).setUpClass()
+        cls.ai = AzimuthalIntegrator(1.0, detector="Pilatus100k")
+        cls.ai.wavelength = 1e-10
+        cls.npt = 100
+        cls.data = numpy.random.random(cls.ai.detector.shape)
 
-    def tearDown(self):
-        unittest.TestCase.tearDown(self)
-        self.data = self.ai = self.npt = None
+    @classmethod
+    def tearDownClass(cls):
+        super(TestPickle, cls).tearDownClass()
+        cls.data = cls.ai = cls.npt = None
 
     def test_Detector_pickle(self):
         det = self.ai.detector  # type: Detector
+        dets = dumps(det)
+        self.assert_(dets, "pickle works")
+        rest = loads(dets)
+        self.assert_(rest, "unpickle works")
+        self.assertEqual(rest.shape, self.ai.detector.MAX_SHAPE)
 
-        print(det, type(det))
-        assert dumps(det)
-        assert loads(dumps(det))
-        self.assertEqual(loads(dumps(det)).shape, Pilatus1M.MAX_SHAPE)
-
+        # test the binning
+        mar = detector_factory("RayonixMx225")
+        mar.guess_binning((2048, 2048))
+        self.assertEqual(mar.binning, (3, 3), "binning OK")
+        mars = dumps(mar)
+        marr = loads(mars)
+        self.assertEqual(mar.binning, marr.binning, "restored binning OK")
 
     def test_AzimuthalIntegrator_pickle(self):
-        spectra = self.ai.integrate1d(self.data, 1000)  # force lut generation
-        dump = dumps(self.ai)
-        newai = loads(dump)  # type: AzimuthalIntegrator
-        self.assertEqual(newai._cached_array, self.ai._cached_array)
-        self.assertEqual(newai.integrate1d(self.data, 1000), spectra)
+        spectra = self.ai.integrate1d(self.data, self.npt)  # force lut generation
+        ais = dumps(self.ai)
+        newai = loads(ais)  # type: AzimuthalIntegrator
+        self.assertEqual(newai._cached_array.keys(), self.ai._cached_array.keys())
+        for key in self.ai._cached_array.keys():
+            if isinstance(self.ai._cached_array[key], numpy.ndarray):
+                self.assertEqual(abs(newai._cached_array[key] - self.ai._cached_array[key]).max(), 0,
+                                 "key %s is the same" % key)
+            else:
+                self.assertEqual(newai._cached_array[key], self.ai._cached_array[key],
+                                 "key %s is the same: %s %s" %
+                                 (key, newai._cached_array[key], self.ai._cached_array[key]))
+        for first, second in zip(newai.integrate1d(self.data, self.npt), spectra):
+            self.assertEqual(abs(first - second).max(), 0, "Spectra are the same")
 
     def test_Calibrant(self):
         from pyFAI import calibrant
         calibrant = calibrant.CalibrantFactory()('AgBh')
         assert dumps(calibrant)
         assert loads(dumps(calibrant))
+
 
 def suite():
     loader = unittest.defaultTestLoader.loadTestsFromTestCase
