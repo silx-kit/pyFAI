7a5884686b1ab391b73097523d924f7e697cea22
Valentin Valls <valentin.valls@esrf.fr>
2018-08-24 17:45:03 +0200

    Fix the way the model is set to the app

--- pyFAI/gui/calibration/CalibrationWindow.py
+++ pyFAI/gui/calibration/CalibrationWindow.py
@@ -27,7 +27,7 @@ from __future__ import absolute_import
 
 __authors__ = ["V. Valls"]
 __license__ = "MIT"
-__date__ = "17/08/2018"
+__date__ = "24/08/2018"
 
 from silx.gui import qt
 import pyFAI.utils
@@ -40,18 +40,19 @@ class CalibrationWindow(qt.QMainWindow):
         context.setParent(self)
         qt.loadUi(pyFAI.utils.get_ui_file("calibration-main.ui"), self)
         self.__context = context
-        self.__model = context.getCalibrationModel()
+        model = context.getCalibrationModel()
 
         context.restoreWindowLocationSettings("main-window", self)
 
         self.__tasks = self.createTasks()
         for task in self.__tasks:
-            task.setModel(self.__model)
             task.nextTaskRequested.connect(self.nextTask)
             self._list.addItem(task.windowTitle())
             self._stack.addWidget(task)
         if len(self.__tasks) > 0:
             self._list.setCurrentRow(0)
+
+        self.setModel(model)
 
     def closeEvent(self, event):
         for task in self.__tasks:
