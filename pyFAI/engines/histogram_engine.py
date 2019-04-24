a8b01be4b5cfdfe67ff9e4b2c9588575d3aabf4c
Jerome Kieffer <jerome.kieffer@esrf.fr>
2019-04-24 15:37:29 +0200

    Provide 2D integration based on numpy histograms

--- pyFAI/engines/histogram_engine.py
+++ pyFAI/engines/histogram_engine.py
@@ -29,7 +29,7 @@ __author__ = "Jerome Kieffer"
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "23/04/2019"
+__date__ = "24/04/2019"
 __status__ = "development"
 
 import logging
@@ -113,20 +113,125 @@ def histogram1d_engine(radial, npt,
                    poissonian=poissonian,
                    empty=0
                    )
-    print(prep.dtype, prep.shape)
     radial = radial.ravel()
     prep.shape = -1, 4
-    histo_signal, position = numpy.histogram(radial, npt, weights=prep[0], range=radial_range)
-    histo_variance, position = numpy.histogram(radial, npt, weights=prep[1], range=radial_range)
-    histo_normalization, position = numpy.histogram(radial, npt, weights=prep[2], range=radial_range)
-    histo_count, position = numpy.histogram(radial, npt, weights=prep[3], range=radial_range)
+    assert prep.shape[0] == radial.size
+    histo_signal, position = numpy.histogram(radial, npt, weights=prep[:, 0], range=radial_range)
+    if variance is not None or poissonian:
+        histo_variance, position = numpy.histogram(radial, npt, weights=prep[:, 1], range=radial_range)
+    else:
+        histo_variance = None
+    histo_normalization, position = numpy.histogram(radial, npt, weights=prep[:, 2], range=radial_range)
+    histo_count, position = numpy.histogram(radial, npt, weights=prep[:, 3], range=radial_range)
     positions = (position[1:] + position[:-1]) / 2.0
     with numpy.errstate(divide='ignore'):
         intensity = histo_variance / histo_normalization
-        error = numpy.sqrt(histo_variance) / histo_normalization
-    empty_bins = numpy.where(histo_count == 0)
+        if histo_variance is None:
+            error = None
+        else:
+            error = numpy.sqrt(histo_variance) / histo_normalization
+    mask_empty = histo_count == 0
     if dummy is not None:
         empty = dummy
-    intensity[empty_bins] = empty
-    error[empty_bins] = empty
+    intensity[mask_empty] = empty
+    if error is not None:
+        error[mask_empty] = empty
     return Integrate1dtpl(positions, intensity, error, histo_signal, histo_variance, histo_normalization, histo_count)
+
+
+def histogram2d_engine(radial, azimuthal, npt,
+                       raw,
+                       dark=None,
+                       flat=None,
+                       solidangle=None,
+                       polarization=None,
+                       absorption=None,
+                       mask=None,
+                       dummy=None,
+                       delta_dummy=None,
+                       normalization_factor=1.0,
+                       empty=None,
+                       split_result=False,
+                       variance=None,
+                       dark_variance=None,
+                       poissonian=False,
+                       radial_range=None,
+                       azimuth_range=None
+                       ):
+    """Implementation of 2D rebinning engine using pure numpy histograms
+    
+    :param radial: radial position 2D array (same shape as raw)
+    :param azimuthal: azimuthal position 2D array (same shape as raw)
+    :param npt: number of points to integrate over in (azimuthal, radial) dimension
+    :param raw: 2D array with the raw signal
+    :param dark: array containing the value of the dark noise, to be subtracted
+    :param flat: Array containing the flatfield image. It is also checked for dummies if relevant.
+    :param solidangle: the value of the solid_angle. This processing may be performed during the rebinning instead. left for compatibility
+    :param polarization: Correction for polarization of the incident beam
+    :param absorption: Correction for absorption in the sensor volume
+    :param mask: 2d array of int/bool: non-null where data should be ignored
+    :param dummy: value of invalid data
+    :param delta_dummy: precision for invalid data
+    :param normalization_factor: final value is divided by this
+    :param empty: value to be given for empty bins
+    :param variance: provide an estimation of the variance
+    :param dark_variance: provide an estimation of the variance of the dark_current,
+    :param poissonian: set to "True" for assuming the detector is poissonian and variance = raw + dark
+
+
+    NaN are always considered as invalid values
+
+    if neither empty nor dummy is provided, empty pixels are left at 0.
+    
+    Nota: "azimuthal_range" has to be integrated into the 
+           mask prior to the call of this function 
+    
+    :return: Integrate1dtpl named tuple containing: 
+            position, average intensity, std on intensity, 
+            plus the various histograms on signal, variance, normalization and count.  
+                                               
+    """
+    prep = preproc(raw,
+                   dark=dark,
+                   flat=flat,
+                   solidangle=solidangle,
+                   polarization=polarization,
+                   absorption=absorption,
+                   mask=mask,
+                   dummy=dummy,
+                   delta_dummy=delta_dummy,
+                   normalization_factor=normalization_factor,
+                   split_result=True,
+                   variance=variance,
+                   dark_variance=dark_variance,
+                   poissonian=poissonian,
+                   empty=0
+                   )
+    radial = radial.ravel()
+    azimuthal = radial.ravel()
+    prep.shape = -1, 4
+    assert prep.shape[0] == radial.size
+    assert prep.shape[0] == azimuthal.size
+    histo_signal, position_azim, position_rad = numpy.histogram2d(azimuthal, radial, npt, weights=prep[:, 0], range=[azimuth_range, radial_range])
+    if variance is not None or poissonian:
+        histo_variance, position_azim, position_rad = numpy.histogram2d(azimuthal, radial, npt, weights=prep[:, 1], range=[azimuth_range, radial_range])
+    else:
+        histo_variance = None
+    histo_normalization, position_azim, position_rad = numpy.histogram2d(azimuthal, radial, npt, weights=prep[:, 2], range=[azimuth_range, radial_range])
+    histo_count, position_azim, position_rad = numpy.histogram2d(azimuthal, radial, npt, weights=prep[:, 3], range=[azimuth_range, radial_range])
+
+    bins_azim = (position_azim[1:] + position_azim[:-1]) / 2.0
+    bins_rad = (position_rad[1:] + position_rad[:-1]) / 2.0
+    with numpy.errstate(divide='ignore'):
+        intensity = histo_variance / histo_normalization
+        if histo_variance is None:
+            error = None
+        else:
+            error = numpy.sqrt(histo_variance) / histo_normalization
+    mask_empty = histo_count == 0
+    if dummy is not None:
+        empty = dummy
+    intensity[mask_empty] = empty
+    if error is not None:
+        error[mask_empty] = empty
+    return Integrate2dtpl(bins_rad, bins_azim, intensity, error, histo_signal, histo_variance, histo_normalization, histo_count)
