7f1a7a46b67abbfc23acf732f94a611b4f653e3f
Jerome Kieffer <jerome.kieffer@esrf.fr>
2019-04-30 14:13:21 +0200

    Single histogram to rull them all

--- pyFAI/ext/histogram.pyx
+++ pyFAI/ext/histogram.pyx
@@ -4,7 +4,7 @@
 #             https://github.com/silx-kit/pyFAI
 #
 #
-#    Copyright (C) 2014-2018 European Synchrotron Radiation Facility, Grenoble, France
+#    Copyright (C) 2012-2019 European Synchrotron Radiation Facility, Grenoble, France
 #
 #    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
 #
@@ -26,34 +26,141 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #  THE SOFTWARE.
 
-"""Re-implementation of numpy histograms without OpenMP"""
+"""A set of histogram functions with or without OpenMP enabled."""
 
 __author__ = "Jerome Kieffer"
-__date__ = "15/01/2019"
-__license__ = "MIY"
-__copyright__ = "2011-2016, ESRF"
+__date__ = "30/04/2019"
+__license__ = "MIT"
+__copyright__ = "2011-2019, ESRF"
 __contact__ = "jerome.kieffer@esrf.fr"
+
 
 include "regrid_common.pxi"
 
+import cython
+from cython.parallel cimport prange
 from libc.math cimport floor, sqrt
+cimport pyFAI.ext._openmp as _openmp
+
 import logging
 logger = logging.getLogger(__name__)
+
+from .preproc import preproc
+
+_COMPILED_WITH_OPENMP = _openmp.COMPILED_WITH_OPENMP
 
 
 @cython.cdivision(True)
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def histogram(cnumpy.ndarray pos,
-              cnumpy.ndarray weights,
-              int bins=100,
-              bin_range=None,
-              pixelSize_in_Pos=None,
-              nthread=None,
-              double empty=0.0,
-              double normalization_factor=1.0):
+def _histogram_omp(cnumpy.ndarray pos not None,
+                   cnumpy.ndarray weights not None,
+                   int bins=100,
+                   bin_range=None,
+                   pixelSize_in_Pos=None,
+                   int nthread=0,
+                   double empty=0.0,
+                   double normalization_factor=1.0):
     """
     Calculates histogram of pos weighted by weights
+    Multi threaded implementation
+
+    :param pos: 2Theta array
+    :param weights: array with intensities
+    :param bins: number of output bins
+    :param pixelSize_in_Pos: size of a pixels in 2theta: DESACTIVATED
+    :param nthread: OpenMP is disabled. unused
+    :param empty: value given to empty bins
+    :param normalization_factor: divide the result by this value
+
+    :return: 2theta, I, weighted histogram, raw histogram
+    """
+
+    assert pos.size == weights.size
+    assert bins > 1
+    cdef:
+        int  size = pos.size
+        position_t[::1] cpos = numpy.ascontiguousarray(pos.ravel(), dtype=position_d)
+        data_t[::1] cdata = numpy.ascontiguousarray(weights.ravel(), dtype=data_d)
+        acc_t[::1] out_data = numpy.zeros(bins, dtype=acc_d)
+        acc_t[::1] out_count = numpy.zeros(bins, dtype=acc_d)
+        data_t[::1] out_merge = numpy.zeros(bins, dtype=data_d)
+        position_t delta, min0, max0, maxin0
+        position_t a = 0.0
+        position_t d = 0.0
+        position_t fbin = 0.0
+        position_t tmp_count, tmp_data = 0.0
+        position_t epsilon = 1e-10
+        int bin = 0, i, idx, thread
+    if pixelSize_in_Pos:
+        logger.warning("No pixel splitting in histogram")
+
+    if bin_range is not None:
+        min0 = min(bin_range)
+        maxin0 = max(bin_range)
+    else:
+        min0 = pos.min()
+        maxin0 = pos.max()
+    max0 = calc_upper_bound(maxin0)
+    delta = (max0 - min0) / float(bins)
+
+    if not nthread:
+        nthread = _openmp.omp_get_max_threads()
+    _openmp.omp_set_num_threads(<int> nthread)
+    cdef:
+        acc_t[:, ::1] big_count = numpy.zeros((nthread, bins), dtype=acc_d)
+        acc_t[:, ::1] big_data = numpy.zeros((nthread, bins), dtype=acc_d)
+
+    if pixelSize_in_Pos:
+        logger.warning("No pixel splitting in histogram")
+
+    with nogil:
+        for i in prange(size):
+            d = cdata[i]
+            a = cpos[i]
+            fbin = get_bin_number(a, min0, delta)
+            bin = < int > fbin
+            if bin < 0 or bin >= bins:
+                continue
+            thread = _openmp.omp_get_thread_num()
+            big_count[thread, bin] += 1.0
+            big_data[thread, bin] += d
+
+        for idx in prange(bins):
+            tmp_count = 0.0
+            tmp_data = 0.0
+            for thread in range(nthread):
+                tmp_count = tmp_count + big_count[thread, idx]
+                tmp_data = tmp_data + big_data[thread, idx]
+            out_count[idx] += tmp_count
+            out_data[idx] += tmp_data
+            if out_count[idx] > epsilon:
+                out_merge[idx] += tmp_data / tmp_count / normalization_factor
+            else:
+                out_merge[idx] += empty
+
+    out_pos = numpy.linspace(min0 + (0.5 * delta), max0 - (0.5 * delta), bins)
+
+    return (out_pos, 
+            numpy.asarray(out_merge), 
+            numpy.asarray(out_data), 
+            numpy.asarray(out_count))
+
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def _histogram_nomp(cnumpy.ndarray pos,
+                    cnumpy.ndarray weights,
+                    int bins=100,
+                    bin_range=None,
+                    pixelSize_in_Pos=None,
+                    nthread=None,
+                    double empty=0.0,
+                    double normalization_factor=1.0):
+    """
+    Calculates histogram of pos weighted by weights,
+    Single threaded implementation
 
     :param pos: 2Theta array
     :param weights: array with intensities
@@ -119,18 +226,23 @@ def histogram(cnumpy.ndarray pos,
             numpy.asarray(out_data), 
             numpy.asarray(out_count))
 
+if _COMPILED_WITH_OPENMP:
+    histogram = _histogram_omp
+else:
+    histogram = _histogram_nomp
+
 
 @cython.cdivision(True)
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def histogram2d(cnumpy.ndarray pos0,
-                cnumpy.ndarray pos1,
-                bins,
-                cnumpy.ndarray weights,
+def histogram2d(cnumpy.ndarray pos0 not None,
+                cnumpy.ndarray pos1 not None,
+                bins not None,
+                cnumpy.ndarray weights not None,
                 split=False,
                 nthread=None,
-                double empty=0.0,
-                double normalization_factor=1.0):
+                data_t empty=0.0,
+                data_t normalization_factor=1.0):
     """
     Calculate 2D histogram of pos0,pos1 weighted by weights
 
@@ -139,11 +251,14 @@ def histogram2d(cnumpy.ndarray pos0,
     :param weights: array with intensities
     :param bins: number of output bins int or 2-tuple of int
     :param split: pixel splitting is disabled in histogram
-    :param nthread: OpenMP is disabled. unused here
+    :param nthread: Unused ! see below
     :param empty: value given to empty bins
     :param normalization_factor: divide the result by this value
 
     :return: I, bin_centers0, bin_centers1, weighted histogram(2D), unweighted histogram (2D)
+
+    Nota: the histogram itself is not parallelized as it is slower than in serial mode 
+    (cache contention)
     """
     assert pos0.size == pos1.size
     assert pos0.size == weights.size
@@ -171,8 +286,7 @@ def histogram2d(cnumpy.ndarray pos0,
         position_t max1 = calc_upper_bound(<position_t> pos1.max())
         position_t delta0 = (max0 - min0) / float(bins0)
         position_t delta1 = (max1 - min1) / float(bins1)
-        position_t fbin0, fbin1, p0, p1
-        acc_t d
+        position_t fbin0, fbin1, p0, p1, d
         position_t epsilon = 1e-10
 
     if split:
@@ -184,22 +298,23 @@ def histogram2d(cnumpy.ndarray pos0,
         for i in range(size):
             p0 = cpos0[i]
             p1 = cpos1[i]
-            d = data[i]
+            
             fbin0 = get_bin_number(p0, min0, delta0)
             fbin1 = get_bin_number(p1, min1, delta1)
             bin0 = < int > floor(fbin0)
             bin1 = < int > floor(fbin1)
             if (bin0 < 0) or (bin1 < 0) or (bin0 >= bins0) or (bin1 >= bins1):
                 continue
+            d = data[i]
             out_count[bin0, bin1] += 1.0
             out_data[bin0, bin1] += d
 
-        for i in range(bins0):
+        for i in prange(bins0):
             for j in range(bins1):
                 if out_count[i, j] > epsilon:
-                    out_merge[i, j] = out_data[i, j] / out_count[i, j] / normalization_factor
+                    out_merge[i, j] += out_data[i, j] / out_count[i, j] / normalization_factor
                 else:
-                    out_merge[i, j] = empty
+                    out_merge[i, j] += empty
 
     return (numpy.asarray(out_merge), 
             bin_centers0, bin_centers1, 
@@ -214,7 +329,6 @@ def histogram_preproc(cnumpy.ndarray pos,
                       cnumpy.ndarray weights,
                       int bins=100,
                       bin_range=None,
-                      pixelSize_in_Pos=None,
                       nthread=None,
                       double empty=0.0,
                       double normalization_factor=1.0):
@@ -254,9 +368,6 @@ def histogram_preproc(cnumpy.ndarray pos,
     if (nchan == 3):
         out_error = numpy.zeros(bins, dtype=data_d)
     
-    if pixelSize_in_Pos:
-        logger.warning("No pixel splitting in histogram")
-
     if bin_range is not None:
         min0 = min(bin_range)
         maxin0 = max(bin_range)
@@ -416,3 +527,72 @@ def histogram2d_preproc(cnumpy.ndarray pos0 not None,
                                    bin_centers1,
                                    numpy.asarray(out_data).view(dtype=prop_d).reshape(bins0, bins1))
     return result
+
+
+def histogram1d_engine(radial, npt,
+                       raw,
+                       dark=None,
+                       flat=None,
+                       solidangle=None,
+                       polarization=None,
+                       absorption=None,
+                       mask=None,
+                       dummy=None,
+                       delta_dummy=None,
+                       normalization_factor=1.0,
+                       empty=None,
+                       split_result=False,
+                       variance=None,
+                       dark_variance=None,
+                       poissonian=False,
+                       radial_range=None
+                       ):
+    """Implementation of rebinning engine (without splitting) using pure cython histograms
+    
+    :param radial: radial position 2D array (same shape as raw)   
+    :param npt: number of points to integrate over
+    :param raw: 2D array with the raw signal
+    :param dark: array containing the value of the dark noise, to be subtracted
+    :param flat: Array containing the flatfield image. It is also checked for dummies if relevant.
+    :param solidangle: the value of the solid_angle. This processing may be performed during the rebinning instead. left for compatibility
+    :param polarization: Correction for polarization of the incident beam
+    :param absorption: Correction for absorption in the sensor volume
+    :param mask: 2d array of int/bool: non-null where data should be ignored
+    :param dummy: value of invalid data
+    :param delta_dummy: precision for invalid data
+    :param normalization_factor: final value is divided by this
+    :param empty: value to be given for empty bins
+    :param variance: provide an estimation of the variance
+    :param dark_variance: provide an estimation of the variance of the dark_current,
+    :param poissonian: set to "True" for assuming the detector is poissonian and variance = raw + dark
+
+
+    NaN are always considered as invalid values
+
+    if neither empty nor dummy is provided, empty pixels are left at 0.
+    
+    Nota: "azimuthal_range" has to be integrated into the 
+           mask prior to the call of this function 
+    
+    :return: Integrate1dtpl named tuple containing: 
+            position, average intensity, std on intensity, 
+            plus the various histograms on signal, variance, normalization and count.  
+                                               
+    """
+    prep = preproc(raw,
+                   dark=dark,
+                   flat=flat,
+                   solidangle=solidangle,
+                   polarization=polarization,
+                   absorption=absorption,
+                   mask=mask,
+                   dummy=dummy,
+                   delta_dummy=delta_dummy,
+                   normalization_factor=normalization_factor,
+                   split_result=4,
+                   variance=variance,
+                   dark_variance=dark_variance,
+                   poissonian=poissonian,
+                   empty=0
+                   )
+    #TODO...#
