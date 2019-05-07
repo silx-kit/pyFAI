646a25037c920966c837ae44548e87e2226380f8
Valentin Valls <valentin.valls@esrf.fr>
2019-05-07 14:20:33 +0200

    Display the exception using a dialog

--- pyFAI/gui/tasks/PeakPickingTask.py
+++ pyFAI/gui/tasks/PeakPickingTask.py
@@ -27,7 +27,7 @@ from __future__ import absolute_import
 
 __authors__ = ["V. Valls"]
 __license__ = "MIT"
-__date__ = "16/04/2019"
+__date__ = "07/05/2019"
 
 import logging
 import numpy
@@ -57,6 +57,7 @@ from ..utils import validators
 from ..helper import model_transform
 from ..widgets.ColoredCheckBox import ColoredCheckBox
 from ..widgets.AdvancedSpinBox import AdvancedSpinBox
+from ..dialog import MessageBox
 
 
 _logger = logging.getLogger(__name__)
@@ -1127,9 +1128,7 @@ class PeakPickingTask(AbstractCalibrationTask):
                 self.__undoStack.push(command)
                 command.setRedoInhibited(False)
             except Exception as e:
-                _logger.error(str(e))
-                _logger.error("Backtrace", exc_info=True)
-                # FIXME Display error dialog
+                MessageBox.exception(self, "Error while loading peaks", e, _logger)
             except KeyboardInterrupt:
                 raise
 
@@ -1147,9 +1146,7 @@ class PeakPickingTask(AbstractCalibrationTask):
             controlPoints = model_transform.createControlPoints(self.model())
             controlPoints.save(filename)
         except Exception as e:
-            _logger.error(str(e))
-            _logger.error("Backtrace", exc_info=True)
-            # FIXME Display error dialog
+            MessageBox.exception(self, "Error while saving peaks", e, _logger)
         except KeyboardInterrupt:
             raise
 
