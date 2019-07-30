86ac05b10fa52c8c43ce971efb4c1511d5fe7cfd
Jerome Kieffer <jerome.kieffer@esrf.fr>
2019-07-30 17:47:30 +0200

    typo related to badly configured eclipse

--- pyFAI/azimuthalIntegrator.py
+++ pyFAI/azimuthalIntegrator.py
@@ -4,7 +4,7 @@
 #    Project: Azimuthal integration
 #             https://github.com/silx-kit/pyFAI
 #
-#    Copyright (C) 2012-2018 European Synchrotron Radiation Facility, Grenoble, France
+#    Copyright (C) 2012-2019 European Synchrotron Radiation Facility, Grenoble, France
 #
 #    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
 #
@@ -32,7 +32,7 @@ __author__ = "Jérôme Kieffer"
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "30/38/2019"
+__date__ = "30/07/2019"
 __status__ = "stable"
 __docformat__ = 'restructuredtext'
 
