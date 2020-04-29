2b73474f18a67ee9515b410208356c93f23051f0
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2020-04-29 15:03:09 +0200

    typo

--- pyFAI/ext/histogram.pyx
+++ pyFAI/ext/histogram.pyx
@@ -8,7 +8,7 @@
 #             https://github.com/silx-kit/pyFAI
 #
 #
-#    Copyright (C) 2012-20120 European Synchrotron Radiation Facility, Grenoble, France
+#    Copyright (C) 2012-2020 European Synchrotron Radiation Facility, Grenoble, France
 #
 #    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
 #
