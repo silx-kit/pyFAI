add6ad06e7c36828d6ccf987bad07dbbe8b69ab1
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2020-10-30 09:33:29 +0100

    Switch to the RTD theme as suggested

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -57,7 +57,8 @@ extensions = [
     'sphinx.ext.todo',
     'sphinxcontrib.programoutput',
     'sphinx.ext.mathjax',
-    'nbsphinx'
+    'nbsphinx',
+    'sphinx_rtd_theme'
 ]
 
 if sphinx.__version__ < "1.4":
@@ -129,7 +130,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
