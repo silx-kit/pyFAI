39d336f753c2a2ac6c85c0e4b78c3fda6a171d1f
Jérôme Kieffer <jerome.kieffer@terre-adelie.org>
2021-01-06 16:09:31 +0100

    use a shallow copy

--- ci/install_pyopencl.sh
+++ ci/install_pyopencl.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 # Compile & install pyopencl
-git clone https://github.com/pyopencl/pyopencl
+git clone --depth=1 --shallow-submodules  https://github.com/pyopencl/pyopencl
 pushd pyopencl
-python  configure.py
-python setup.py build bdist_wheel
+pip install pybind11 mako
+python3  configure.py
+python3 setup.py build bdist_wheel
 pip install --pre dist/*.whl
 popd
