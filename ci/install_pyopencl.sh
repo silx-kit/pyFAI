38da39ebc8f5b210558f5020b0ff9e92f53a6e06
Jérôme Kieffer <jerome.kieffer@terre-adelie.org>
2021-01-06 16:30:34 +0100

    use provided headers and lib

--- ci/install_pyopencl.sh
+++ ci/install_pyopencl.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Compile & install pyopencl
-git clone --depth=1 --shallow-submodules  https://github.com/pyopencl/pyopencl
+git clone --depth=1 --shallow-submodules --recurse-submodules https://github.com/pyopencl/pyopencl
 pushd pyopencl
 pip install pybind11 mako
-python3  configure.py
+CL_LIBRARY_PATH=${python3 -c "import os; print(os.environ['LD_LIBRARY_PATH'].split(':')[-1])"}
+CL_INCLUDE_PATH=${python3 -c "import os; print(os.environ['C_INCLUDE_PATH'].split(':')[-1])"}
+python3  configure.py  --cl-inc-dir=${CL_INCLUDE_PATH} --cl-lib-dir=${CL_LIBRARY_PATH}
 python3 setup.py build bdist_wheel
 pip install --pre dist/*.whl
 popd
