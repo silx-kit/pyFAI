cbb58df5f2cdb6b754459fc288a1bb3f06660448
Jerome Kieffer <jerome.kieffer@esrf.fr>
2021-10-28 18:15:20 +0200

    Test for single threaded versions
    used by Apple CPU driver

--- pyFAI/opencl/test/test_peak_finder.py
+++ pyFAI/opencl/test/test_peak_finder.py
@@ -33,7 +33,7 @@ __authors__ = ["Jérôme Kieffer"]
 __contact__ = "jerome.kieffer@esrf.eu"
 __license__ = "MIT"
 __copyright__ = "2020 European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "27/09/2021"
+__date__ = "28/10/2021"
 
 import logging
 import numpy
@@ -100,6 +100,32 @@ class TestOclPeakFinder(unittest.TestCase):
         lut = engine.lut
         distance = self.ai._cached_array["r_center"]
         pf = OCL_PeakFinder(lut, self.img.size, unit=unit, radius=distance, bin_centers=bin_centers, mask=msk)
+        res = pf(self.img, error_model="poisson", dummy=-1)
+        s1 = set((i["x"], i["y"]) for i in self.ref)
+        s2 = set(zip(res.x, res.y))
+        self.assertGreater(len(s2), len(s1), "Many more peaks with default settings")
+        self.assertFalse(bool(s1.difference(s1.intersection(s2))), "All peaks found")
+        # Test densification function
+        dense = densify(res)
+        self.assertLess(abs(dense - self.img).max(), 20, "max difference is contained")
+        self.assertLess(abs((dense - self.img).mean()), 1, "mean of difference is close to zero")
+        self.assertLess((dense - self.img).std(), 3, "standard deviation of difference is contained")
+
+    @unittest.skipUnless(ocl, "pyopencl is missing")
+    def test_azimuthal_peak_finder_single(self):
+        """
+        test for peak picker with background calculate from an azimuthal sigma-clipping
+
+        single threaded version
+        """
+        unit = "r_m"
+        msk = self.img < 0
+        engine = self.ai.setup_CSR(self.img.shape, 128, mask=msk, split="no", unit=unit)
+        bin_centers = engine.bin_centers
+        lut = engine.lut
+        distance = self.ai._cached_array["r_center"]
+        pf = OCL_PeakFinder(lut, self.img.size, unit=unit, radius=distance, bin_centers=bin_centers, mask=msk,
+                            block_size=1)
         res = pf(self.img, error_model="poisson", dummy=-1)
         s1 = set((i["x"], i["y"]) for i in self.ref)
         s2 = set(zip(res.x, res.y))
