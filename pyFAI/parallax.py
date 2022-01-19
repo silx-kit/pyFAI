dcad04180c83fe8df85c469c51c0b1bb052f56cd
Jérôme Kieffer <jerome.kieffer@terre-adelie.org>
2022-01-19 08:50:35 +0100

    typo

--- pyFAI/parallax.py
+++ pyFAI/parallax.py
@@ -29,7 +29,7 @@ __author__ = "Jérôme Kieffer"
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "26/08/2021"
+__date__ = "19/01/2022"
 __status__ = "development"
 __docformat__ = 'restructuredtext'
 
@@ -41,7 +41,7 @@ import scipy.integrate, scipy.signal
 from math import sin, cos, pi, log, sqrt
 from .utils.decorators import timeit
 EPS = numpy.finfo("float64").eps
-OVERSAMPLING = 1024 # Default oversampling value
+OVERSAMPLING = 1024  # Default oversampling value
 
 
 class Beam:
@@ -58,26 +58,26 @@ class Beam:
         self.pos = None
         self.intensity = None
         self.funct = self.__getattribute__(self.profile)
-         
+
     def __repr__(self):
         return f"Beam of shape {self.profile} with a width of {self.width}m"
-    
+
     def get_config(self):
         """Gets the configuration as a dictionnary"""
         return {"class": self.__class__.__name__,
-                "width": self.width, 
+                "width": self.width,
                 "profile":self.profile}
-    
+
     def set_config(self, cfg):
         """Set the configuration from a dictionnary"""
         if "class" in cfg:
-            assert cfg["class"]==self.__class__.__name__
+            assert cfg["class"] == self.__class__.__name__
         self.width = cfg.get("width")
         self.profile = cfg.get("profile")
         self.pos = None
         self.intensity = None
-        self.funct = self.__getattribute__(self.profile) 
-        
+        self.funct = self.__getattribute__(self.profile)
+
     def gaussian(self, width, over=None):
         """Model the beam as a gaussian profile.
 
@@ -86,13 +86,13 @@ class Beam:
         :return: position, intensity (normalized)
         """
         over = over or OVERSAMPLING
-        if width<EPS:
+        if width < EPS:
             print("Warning, width too small")
             width = EPS
         step = width / over
-        sigma = width/(2.0*sqrt(2.0*log(2.0)))
-        nsteps = 2*int(3*sigma/step+1) + 1
-        pos = (numpy.arange(nsteps) - nsteps//2) * step
+        sigma = width / (2.0 * sqrt(2.0 * log(2.0)))
+        nsteps = 2 * int(3 * sigma / step + 1) + 1
+        pos = (numpy.arange(nsteps) - nsteps // 2) * step
         peak = numexpr.evaluate("exp(-pos**2/(2*(sigma**2)))")
         peak /= peak.sum()
         return pos, peak
@@ -105,12 +105,12 @@ class Beam:
         :return: position, intensity (normalized)
         """
         over = over or OVERSAMPLING
-        if width<EPS:
+        if width < EPS:
             print("Warning, width too small")
             width = EPS
         step = width / over
-        nsteps = 2*int(2*width/step+1) + 1
-        pos = (numpy.arange(nsteps) - nsteps//2) * step
+        nsteps = 2 * int(2 * width / step + 1) + 1
+        pos = (numpy.arange(nsteps) - nsteps // 2) * step
         peak = numexpr.evaluate("where(abs(pos)<=width/2, 1.0, 0.0)")
         peak /= peak.sum()
         return pos, peak
@@ -123,16 +123,16 @@ class Beam:
         :return: position, intensity (normalized)
         """
         over = over or OVERSAMPLING
-        if width<EPS:
+        if width < EPS:
             print("Warning, width too small")
             width = EPS
         step = width / over
-        nsteps = 2*int(width/step+2) + 1
-        pos = (numpy.arange(nsteps) - nsteps//2) * step
+        nsteps = 2 * int(width / step + 2) + 1
+        pos = (numpy.arange(nsteps) - nsteps // 2) * step
         peak = numexpr.evaluate("where(abs(pos)<=width/2, sqrt(1.0-(2.0*pos/width)**2), 0.0)")
         peak /= peak.sum()
         return pos, peak
-    
+
     def __call__(self, width=None, over=None):
         """Return the beam profile I=f(x)"""
         over = over or OVERSAMPLING
@@ -152,13 +152,14 @@ class BaseSensor:
     i.e. the beam sees an exponential decay of the intensity with the traveled distance exp(-µx).
     The inverse of the absorption coefficient, 1/mu, is the "average" penetration distance.
     """
+
     def __init__(self, mu=None):
         """Constructor of the base-sensor.
         
         :param mu: 
         """
         self.mu = mu
-        
+
     def measure_displacement(self, angle=None, beam=None, over=None):
         """Measures the displacement of the peak due to parallax effect
         :param width: FWHM of the peak, same unit as thickness. *Unused*
@@ -166,7 +167,7 @@ class BaseSensor:
         :param beam: shape of the beam. *Unused*
         :param over: oversampling factor. *Unused*
         """
-        return numpy.sin(angle)/self.mu
+        return numpy.sin(angle) / self.mu
 
     def __repr__(self):
         return f"Thick sensor with µ={self.mu} 1/m"
@@ -174,12 +175,12 @@ class BaseSensor:
     def get_config(self):
         """Gets the configuration as a dictionnary"""
         return {"class": self.__class__.__name__,
-                "mu": self.mu,}
-    
+                "mu": self.mu, }
+
     def set_config(self, cfg):
         """Set the configuration from a dictionnary"""
         if "class" in cfg:
-            assert cfg["class"]==self.__class__.__name__
+            assert cfg["class"] == self.__class__.__name__
         self.mu = cfg.get("mu")
         return self
 
@@ -202,7 +203,7 @@ class ThinSensor(BaseSensor):
     Nota: the absortion coefficient depends on the wavelength, but the effect is expected to
     be negligeable when refining the wagelength in a calibration experiment. 
     """
-    
+
     def __init__(self, thickness=None, efficiency=None):
         """Class to simulate the decay of the parallax effect
 
@@ -211,12 +212,12 @@ class ThinSensor(BaseSensor):
         """
         self.thickness = None
         self.efficiency = None
-        if thickness is not None: 
+        if thickness is not None:
             self.thickness = float(thickness)
         if efficiency is not None:
             self.efficiency = float(efficiency)
         if self.thickness and self.efficiency:
-            BaseSensor.__init__(self, - log(1.0-self.efficiency)/self.thickness)
+            BaseSensor.__init__(self, -log(1.0 - self.efficiency) / self.thickness)
         else:
             BaseSensor.__init__(self, None)
         self.formula = numexpr.NumExpr("where(x<0, 0.0, mu*exp(-mu*x))")
@@ -230,16 +231,16 @@ class ThinSensor(BaseSensor):
                 "mu": self.mu,
                 "thickness": self.thickness,
                 "efficiency": self.efficiency}
-    
+
     def set_config(self, cfg):
         """Set the configuration from a dictionnary"""
         if "class" in cfg:
-            assert cfg["class"]==self.__class__.__name__
+            assert cfg["class"] == self.__class__.__name__
         self.mu = cfg.get("mu")
         self.thickness = float(cfg.get("thickness"))
         self.efficiency = float(cfg.get("efficiency"))
         return self
-                                
+
     def __call__(self, x):
         "Calculate the absorption at position x"
         return self.formula(self.mu, x)
@@ -266,11 +267,11 @@ class ThinSensor(BaseSensor):
         :return position (along the detector), absorption (normalized)
         """
         over = over or OVERSAMPLING
-        length = self.thickness/cos(angle)
+        length = self.thickness / cos(angle)
         pos = numpy.linspace(0, length, over)
         decay = self.__call__(pos)
-        decay /= decay.sum()   # Normalize the decay
-        pos *= sin(angle) # rotate the decay to have it in the detector plan:
+        decay /= decay.sum()  # Normalize the decay
+        pos *= sin(angle)  # rotate the decay to have it in the detector plan:
         return pos, decay
 
     def convolve(self, angle, beam, over=None):
@@ -283,24 +284,24 @@ class ThinSensor(BaseSensor):
         """
         over = over or OVERSAMPLING
         pos_dec, decay = self.absorption(angle, over)
-        
-        pos_peak, peak = beam(beam.width/cos(angle), over=over)
-        #Interpolate grids ...
+
+        pos_peak, peak = beam(beam.width / cos(angle), over=over)
+        # Interpolate grids ...
         pos_min = min(pos_dec[0], pos_peak[0])
         pos_max = max(pos_dec[-1], pos_peak[-1])
-        step = min((pos_dec[-1] - pos_dec[0])/(pos_dec.shape[0]-1),
-                   (pos_peak[-1] - pos_peak[0])/(pos_dec.shape[0]-1))
-        if step<EPS:
-            step = max((pos_dec[-1] - pos_dec[0])/(pos_dec.shape[0]-1),
-                       (pos_peak[-1] - pos_peak[0])/(pos_dec.shape[0]-1))
-        nsteps_2 = int(max(-pos_min, pos_max)/step + 0.5)
+        step = min((pos_dec[-1] - pos_dec[0]) / (pos_dec.shape[0] - 1),
+                   (pos_peak[-1] - pos_peak[0]) / (pos_dec.shape[0] - 1))
+        if step < EPS:
+            step = max((pos_dec[-1] - pos_dec[0]) / (pos_dec.shape[0] - 1),
+                       (pos_peak[-1] - pos_peak[0]) / (pos_dec.shape[0] - 1))
+        nsteps_2 = int(max(-pos_min, pos_max) / step + 0.5)
 
-        max_steps = 1<<20
-        if nsteps_2>max_steps:
+        max_steps = 1 << 20
+        if nsteps_2 > max_steps:
             nsteps_2 = max_steps
-            step = (pos_max-pos_min)/(max_steps-1)
+            step = (pos_max - pos_min) / (max_steps - 1)
 
-        pos = (numpy.arange(2*nsteps_2+1) - nsteps_2) *  step
+        pos = (numpy.arange(2 * nsteps_2 + 1) - nsteps_2) * step
         big_decay = numpy.interp(pos, pos_dec, decay, left=0.0, right=0.0)
         dsum = big_decay.sum()
         if dsum == 0:
@@ -320,14 +321,14 @@ class ThinSensor(BaseSensor):
         ax.plot(*c, label="Absorption")
         c = beam()
         ax.plot(*c, label=f"peak w={beam.width*1000} mm")
-        c = beam(beam.width/cos(angle))
+        c = beam(beam.width / cos(angle))
         ax.plot(*c, label=f"peak w={beam.width*1000} mm, inclined")
 
         c = self.convolve(angle, beam=beam)
         ax.plot(*c, label="Convolution")
         idx = numpy.argmax(c[1])
         maxi = self.measure_displacement(angle, beam=beam)
-        ax.annotate(f"$\delta r$={maxi*1000:.3f}mm", (maxi, c[1][idx]),
+        ax.annotate(f"$\\delta r$={maxi*1000:.3f}mm", (maxi, c[1][idx]),
                    xycoords='data',
                    xytext=(0.8, 0.5), textcoords='axes fraction',
                    arrowprops=dict(facecolor='black', shrink=0.05),
@@ -344,29 +345,29 @@ class ThinSensor(BaseSensor):
         """
         over = over or OVERSAMPLING
         angle = abs(angle)
-        if angle >=pi/2.0:
-            return 1.0/self.mu
-        
+        if angle >= pi / 2.0:
+            return 1.0 / self.mu
+
         x, y = self.convolve(angle, beam=beam, over=over)
         ymax = y.max()
-        idx_max = numpy.where(y==ymax)[0]
-        if len(idx_max)>1:
+        idx_max = numpy.where(y == ymax)[0]
+        if len(idx_max) > 1:
             return x[idx_max].mean()
         else:
             idx = idx_max[0]
-            if idx>1 or idx<len(y)-1:
-                #Second order tailor expension
-                f_prime = 0.5*(y[idx+1]-y[idx-1])
-                f_sec = (y[idx+1]+y[idx-1]-2*y[idx])
+            if idx > 1 or idx < len(y) - 1:
+                # Second order tailor expension
+                f_prime = 0.5 * (y[idx + 1] - y[idx - 1])
+                f_sec = (y[idx + 1] + y[idx - 1] - 2 * y[idx])
                 if f_sec == 0:
                     print('f" is null')
                     return x[idx]
-                delta = -f_prime/f_sec
-                if abs(delta)>1:
+                delta = -f_prime / f_sec
+                if abs(delta) > 1:
                     print("Too large displacement")
                     return x[idx]
-                step = (x[-1]-x[0])/(len(x)-1)
-                return x[idx] + delta*step
+                step = (x[-1] - x[0]) / (len(x) - 1)
+                return x[idx] + delta * step
             return x[idx]
 
 
@@ -375,7 +376,8 @@ class Parallax:
     due to parallax effect from the sine of the incidence angle
      
     """
-    SIZE = 64 # <8k  best fits into L1 cache
+    SIZE = 64  # <8k  best fits into L1 cache
+
     def __init__(self, sensor=None, beam=None):
         """Constructor for the Parallax class
         
@@ -392,36 +394,36 @@ class Parallax:
         self.sin_incidence = None
         if self.sensor:
             self.init()
-    
+
     @timeit
     def init(self, over=None):
         """Initialize actually the class...
 
         :param over: enforce the oversampling factor for numerical integration 
         """
-        angles = numpy.linspace(0, pi/2.0, self.SIZE)
+        angles = numpy.linspace(0, pi / 2.0, self.SIZE)
         displacement = [self.sensor.measure_displacement(angle, beam=self.beam, over=over)
                         for angle in angles]
-        self.sin_incidence = numpy.sin(angles) 
+        self.sin_incidence = numpy.sin(angles)
         self.displacement = numpy.array(displacement)
-    
+
     def __repr__(self):
         return f"Parallax correction for {self.beam} and {self.sensor}"
-        
+
     def __call__(self, sin_incidence):
         """Calculate the displacement from the sine of the incidence angle"""
         return numpy.interp(sin_incidence, self.sin_incidence, self.displacement)
-    
+
     def get_config(self):
-        dico ={"class": self.__class__.__name__}
+        dico = {"class": self.__class__.__name__}
         dico["sensor"] = self.sensor.get_config() if self.sensor else None
         dico["beam"] = self.beam.get_config() if self.beam else None
         return dico
-    
+
     def set_config(self, cfg):
         """Set the configuration from a dictionnary"""
         if "class" in cfg:
-            assert cfg["class"]==self.__class__.__name__
+            assert cfg["class"] == self.__class__.__name__
         if "beam" in cfg:
             bfg = cfg["beam"]
             if bfg is None:
@@ -430,10 +432,10 @@ class Parallax:
                 if "class" in bfg:
                     classname = bfg["class"]
                     Klass = globals()[classname]
-                else: 
+                else:
                     Klass = Beam
                 self.beam = Klass()
-                self.beam.set_config(bfg) 
+                self.beam.set_config(bfg)
         if "sensor" in cfg:
             sfg = cfg["sensor"]
             if sfg is None:
@@ -442,7 +444,7 @@ class Parallax:
                 if "class" in sfg:
                     classname = sfg["class"]
                     Klass = globals()[classname]
-                else: 
+                else:
                     Klass = ThinSensor
                 self.sensor = Klass()
                 self.sensor.set_config(sfg)
