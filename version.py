fd63c7d9ba35e687ef5c4ec717c01bf46564572a
Jerome Kieffer <jerome.kieffer@esrf.fr>
2022-02-03 16:04:10 +0100

    switch to development mode

--- version.py
+++ version.py
@@ -61,9 +61,9 @@ RELEASE_LEVEL_VALUE = {"dev": 0,
                        "final": 15}
 
 MAJOR = 0
-MINOR = 21
-MICRO = 2
-RELEV = "final"  # <16
+MINOR = 22
+MICRO = 0
+RELEV = "dev"  # <16
 SERIAL = 0  # <16
 
 date = __date__
