0d794a6ac5683db732d30a6f3d5a34d7a3060e04
Jerome Kieffer <jerome.kieffer@esrf.fr>
2022-04-12 13:52:45 +0200

    switch to dev4 since we are in April

--- version.py
+++ version.py
@@ -47,7 +47,7 @@ Thus 2.1.0a3 is hexversion 0x020100a3.
 __authors__ = ["Jérôme Kieffer", "V. Valls"]
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "03/02/2022"
+__date__ = "12/04/2022"
 __status__ = "production"
 __docformat__ = 'restructuredtext'
 __all__ = ["date", "version_info", "strictversion", "hexversion", "debianversion",
@@ -64,7 +64,7 @@ MAJOR = 0
 MINOR = 22
 MICRO = 0
 RELEV = "dev"  # <16
-SERIAL = 1  # <16
+SERIAL = 4  # <16
 
 date = __date__
 
