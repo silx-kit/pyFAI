dee0060a0037641f096cd1ec8a63c5ad57b75dfe
Jérôme Kieffer <jerome.kieffer@terre-adelie.org>
2022-09-01 09:43:38 +0200

    make exported file compliant with NXmonopd

--- sandbox/nexus_monopd.py
+++ sandbox/nexus_monopd.py
@@ -1,9 +1,12 @@
 import fabio, pyFAI
 import pyFAI.test.utilstest
 from pyFAI.io.nexus import save_NXmonpd
+import os
+dest = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_monopd.nxs")
 
-img=fabio.open(pyFAI.test.utilstest.UtilsTest.getimage("Pilatus1M.edf"))
-ai=pyFAI.load(pyFAI.test.utilstest.UtilsTest.getimage("Pilatus1M.poni"))
+img = fabio.open(pyFAI.test.utilstest.UtilsTest.getimage("Pilatus1M.edf"))
+ai = pyFAI.load(pyFAI.test.utilstest.UtilsTest.getimage("Pilatus1M.poni"))
 
-res = ai.integrate1d(img.data, 1000, unit="2th_deg", error_model="poisson")
-save_NXmonpd("test_monopd.nxs", res, sample="AgBh", instrument="bm26")
+res = ai.integrate1d(img.data, 1000, unit="2th_deg", error_model="poisson", filename="test_monopd.dat")
+save_NXmonpd(dest, res, sample="AgBh", instrument="bm26")
+print(dest, os.path.abspath(__file__))
