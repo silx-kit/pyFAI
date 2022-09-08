77c1302e07fe75ff983935690444942845cbe5ef
Jérôme Kieffer <jerome.kieffer@terre-adelie.org>
2022-09-08 16:58:53 +0200

    polish demo

--- sandbox/csc_debug.py
+++ sandbox/csc_debug.py
@@ -19,7 +19,7 @@ ref = csr.integrate_ng(img.data, dummy=-5)
 res = csc.integrate_ng(img.data, dummy=-5)
 for idx in ("position", 'intensity', 'sigma', 'signal', 'variance', 'normalization', 'count', 'std', 'sem', 'norm_sq'):
     got = numpy.allclose(ref.__getattribute__(idx), res.__getattribute__(idx))
-    print(f"Checking {idx}: {got}")
+    print(f"Checking {idx}: {'OK' if got else 'FAILED'}")
     if not got:
         print(ref.__getattribute__(idx)[:10])
         print(res.__getattribute__(idx)[:10])
@@ -31,12 +31,15 @@ if 1:
     stack = [numpy.random.randint(0, 65000, size=img.shape) for i in range(1000)]
 
     gc.collect()
+    first = None
     for i in range(int(1 + numpy.ceil(numpy.log(multiprocessing.cpu_count()) / numpy.log(2)))):
         t = 1 << i
         t0 = time.perf_counter()
         with ThreadPoolExecutor(max_workers=t) as executor:
             results = [i for i in executor.map(csc.integrate_ng, stack)]
         t1 = time.perf_counter()
-        print(t, t1 - t0)
+        if first is None:
+            first = t1 - t0
+        print(f"{t} threads: {t1 - t0:.3f} ms/img, speed-up: {first/(t1-t0):.3f}x")
         del results
         gc.collect()
