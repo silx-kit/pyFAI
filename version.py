ba9398572d70767692a83c52f8159a301c3e024e
Jerome Kieffer <jerome.kieffer@esrf.fr>
2023-01-25 11:33:14 +0100

    update version number

--- version.py
+++ version.py
@@ -47,7 +47,7 @@ Thus 2.1.0a3 is hexversion 0x020100a3.
 __authors__ = ["Jérôme Kieffer", "V. Valls"]
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "29/12/2022"
+__date__ = "25/01/2023"
 __status__ = "production"
 __docformat__ = 'restructuredtext'
 __all__ = ["date", "version_info", "strictversion", "hexversion", "debianversion",
@@ -62,8 +62,8 @@ RELEASE_LEVEL_VALUE = {"dev": 0,
 
 MAJOR = 2023
 MINOR = 1
-MICRO = 0
-RELEV = "final"  # <16
+MICRO = 1
+RELEV = "dev"  # <16
 SERIAL = 0  # <16
 
 date = __date__
