2a0928340ad6bdcd4df276ae7883580126af2695
Jerome Kieffer <jerome.kieffer@esrf.fr>
2024-01-15 14:40:14 +0100

    start to make a release candidate

--- version.py
+++ version.py
@@ -47,7 +47,7 @@ Thus 2.1.0a3 is hexversion 0x020100a3.
 __authors__ = ["Jérôme Kieffer", "V. Valls"]
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "01/12/2023"
+__date__ = "15/01/2024"
 __status__ = "production"
 __docformat__ = 'restructuredtext'
 __all__ = ["date", "version_info", "strictversion", "hexversion", "debianversion",
@@ -63,7 +63,7 @@ RELEASE_LEVEL_VALUE = {"dev": 0,
 MAJOR = 2024
 MINOR = 1
 MICRO = 0
-RELEV = "dev"  # <16
+RELEV = "rc"  # <16
 SERIAL = 0  # <16
 
 date = __date__
