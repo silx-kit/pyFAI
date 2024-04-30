c72c4124fd095c0e10b43b5cd4749ff0df3954c5
Jerome Kieffer <jerome.kieffer@esrf.fr>
2024-04-30 21:10:39 +0200

    Update test_azimuthal_integrator.py

--- src/pyFAI/test/test_azimuthal_integrator.py
+++ src/pyFAI/test/test_azimuthal_integrator.py
@@ -25,7 +25,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-from tornado.test.web_test import RaiseWithReasonTest
+
 
 """test suite for Azimuthal integrator class"""
 
