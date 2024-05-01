cf9795b5e6dcbc561981a009486202a468430e11
Jerome Kieffer <jerome.kieffer@esrf.fr>
2024-05-01 13:55:23 +0200

    Update azim_hist.py
    remove WIP

--- src/pyFAI/opencl/azim_hist.py
+++ src/pyFAI/opencl/azim_hist.py
@@ -3,7 +3,7 @@
 #    Project: Azimuthal integration
 #             https://github.com/silx-kit/pyFAI
 #
-#    Copyright (C) 2012-2023 European Synchrotron Radiation Facility, Grenoble, France
+#    Copyright (C) 2012-2024 European Synchrotron Radiation Facility, Grenoble, France
 #
 #    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
 #                            D. Karkoulis (dimitris.karkoulis@gmail.com)
@@ -425,7 +425,7 @@ class OCL_Histogram1d(OpenclProcessing):
         :param safe: if True (default) compares arrays on GPU according to their checksum, unless, use the buffer location is used
         :param preprocess_only: return the dark subtracted; flat field & solidangle & polarization corrected image, else
         :param normalization_factor: divide raw signal by this value
-        :param bool weighted_average: set to False to use an unweighted mean (similar to legacy) instead of the weighted average. WIP
+        :param bool weighted_average: set to False to use an unweighted mean (similar to legacy) instead of the weighted average.
         :param radial_range: provide lower and upper bound for radial array
         :param azimuth_range: provide lower and upper bound for azimuthal array
         :param histo_signal: destination array or pyopencl array for sum of signals
@@ -803,7 +803,7 @@ class OCL_Histogram2d(OCL_Histogram1d):
         :param safe: if True (default) compares arrays on GPU according to their checksum, unless, use the buffer location is used
         :param preprocess_only: return the dark subtracted; flat field & solidangle & polarization corrected image, else
         :param normalization_factor: divide raw signal by this value
-        :param bool weighted_average: set to False to use an unweighted mean (similar to legacy) instead of the weighted average. WIP
+        :param bool weighted_average: set to False to use an unweighted mean (similar to legacy) instead of the weighted average.
         :param radial_range: provide lower and upper bound for radial array
         :param azimuth_range: provide lower and upper bound for azimuthal array
         :param histo_signal: destination array or pyopencl array for sum of signals
