28abf02c63f6fd26e4287ec587b493f34aae46ec
edgar <edgar.gutierrez-fernandez@esrf.fr>
2024-08-23 15:24:53 +0200

    blank

--- src/pyFAI/gui/pilx/utils.py
+++ src/pyFAI/gui/pilx/utils.py
@@ -47,7 +47,6 @@ from ...detectors import Detector
 def compute_radial_values(pyFAI_config_as_str: str) -> numpy.ndarray:
     pyFAI_config: dict = json.loads(pyFAI_config_as_str)
     ai = AzimuthalIntegrator.sload(pyFAI_config)
-
     if "detector" not in pyFAI_config and "detector" not in pyFAI_config.get("poni", {}):
         ai.detector = Detector.factory("detector", {
                                       "pixel1": pyFAI_config.get("pixel1"),
