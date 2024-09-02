888f43520e60f247c541b7378a0646718b65755a
Jerome Kieffer <jerome.kieffer@esrf.fr>
2024-09-02 17:18:34 +0200

    investigate the seg-fault in tests

--- ci/intel_opencl_icd.sh
+++ ci/intel_opencl_icd.sh
@@ -4,7 +4,7 @@
 
 URL="http://www.silx.org/pub/OpenCL/"
 FILENAME="intel_opencl_icd-6.4.0.37.tar.gz"
-
+rm -rf $FILENAME
 wget ${URL}${FILENAME}
 tar -xzf $FILENAME
 
@@ -13,4 +13,5 @@ echo $(pwd)/intel_opencl_icd/icd/libintelocl.so > intel_opencl_icd/vendors/intel
 export OCL_ICD_VENDORS=$(pwd)/intel_opencl_icd/vendors
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(pwd)/intel_opencl_icd/lib
 export C_INCLUDE_PATH=${C_INCLUDE_PATH}:$(pwd)/intel_opencl_icd/include
+ldd $(pwd)/intel_opencl_icd/bin/clinfo
 $(pwd)/intel_opencl_icd/bin/clinfo
