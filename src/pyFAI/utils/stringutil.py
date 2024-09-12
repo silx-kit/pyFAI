f78e183ec05f497901ad7799840cd5438be8933e
Jerome Kieffer <jerome.kieffer@esrf.fr>
2024-09-12 16:07:10 +0200

    Wrong pixel size sometimes

--- src/pyFAI/utils/stringutil.py
+++ src/pyFAI/utils/stringutil.py
@@ -28,7 +28,7 @@
 __author__ = "valentin.valls@esrf.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "25/06/2024"
+__date__ = "12/09/2024"
 __status__ = "development"
 __docformat__ = 'restructuredtext'
 
@@ -192,4 +192,8 @@ def to_eng(value, fmt=None, space=""):
             ffmt = "{value:%s}{space}{pfix}"%fmt
             return ffmt.format(value=value, space=space, pfix=pfix, fmt=fmt)
         else:
-            return f"{value:f}".rstrip("0.")+space+pfix
+            str = f"{value:f}"
+            if "." in str:
+                str = str.rstrip("0")
+                str = str.rstrip(".")
+            return str+space+pfix
