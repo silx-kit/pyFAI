0a95c79d8b010b8f9e0368409fa2db2e65cb3e40
Jerome Kieffer <jerome.kieffer@esrf.fr>
2025-01-07 17:36:56 +0100

    Flake8 dislikes this kind of typing.
    Typing is not my business

--- src/pyFAI/io/integration_config.py
+++ src/pyFAI/io/integration_config.py
@@ -502,7 +502,7 @@ class WorkerConfig:
             w.write(json.dumps(self.as_dict(), indent=2))
 
     @classmethod
-    def from_file(cls, filename: str) -> WorkerConfig:
+    def from_file(cls, filename: str):
         """load the content of a JSON file and provide a dataclass instance"""
         with open(filename, "r") as f:
             dico = json.loads(f.read())
