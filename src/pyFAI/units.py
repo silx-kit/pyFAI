8c319306fb9bd0c5119efbafc4474e9f1f59b7e7
edgar1993a <edgar.gutierrez-fernandez@esrf.fr>
2025-01-13 18:27:57 +0100

    formula_so1

--- src/pyFAI/units.py
+++ src/pyFAI/units.py
@@ -186,20 +186,25 @@ class UnitFiber(Unit):
             elif self._sample_orientation == 2:
                 formula_ = self.formula_so1.replace('x', '(-x)')
             elif self._sample_orientation == 3:
+                formula_ = self.formula_so1
                 formula_ = formula_.replace('x', 'ψ').replace('y', 'ξ')
                 formula_ = formula_.replace('ψ', '(-x)').replace('ξ', '(-y)')
             elif self._sample_orientation == 4:
                 formula_ = self.formula_so1.replace('y', '(-y)')
             elif self._sample_orientation == 5:
+                formula_ = self.formula_so1
                 formula_ = formula_.replace('x', 'ψ').replace('y', 'ξ')
                 formula_ = formula_.replace('ψ', '(-y)').replace('ξ', '(-x)')
             elif self._sample_orientation == 6:
+                formula_ = self.formula_so1
                 formula_ = formula_.replace('x', 'ψ').replace('y', 'ξ')
                 formula_ = formula_.replace('ψ', '(-y)').replace('ξ', '(x)')
             elif self._sample_orientation == 7:
+                formula_ = self.formula_so1
                 formula_ = formula_.replace('x', 'ψ').replace('y', 'ξ')
                 formula_ = formula_.replace('ψ', '(y)').replace('ξ', '(x)')
             elif self._sample_orientation == 8:
+                formula_ = self.formula_so1
                 formula_ = formula_.replace('x', 'ψ').replace('y', 'ξ')
                 formula_ = formula_.replace('ψ', '(y)').replace('ξ', '(-x)')
             self.formula = formula_
