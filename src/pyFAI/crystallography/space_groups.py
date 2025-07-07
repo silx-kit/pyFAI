061884fd6dcacdf3c58c88a842e1f9128b6d0887
Gudrun Lotze <64900988+gudlot@users.noreply.github.com>
2025-07-07 12:03:52 +0200

    Validated group35_Cmm2 and reduced code

--- src/pyFAI/crystallography/space_groups.py
+++ src/pyFAI/crystallography/space_groups.py
@@ -437,17 +437,9 @@ class ReflectionCondition:
         - h00 (k=0, l=0):      h even
         - 0k0 (h=0, l=0):      k even
         validated"""
-        if h == 0:
-            if l == 0:
-                return k % 2 == 0    # 0k0
-            return k % 2 == 0        # 0kl
-        if k == 0:
-            if l == 0:
-                return h % 2 == 0    # h00
-            return h % 2 == 0        # h0l
-        if l == 0:
-            return (h + k) % 2 == 0  # hk0
-        return (h + k) % 2 == 0      # general
+        if h == 0 or k == 0:
+            return (h + k) % 2 == 0
+        return (h + k) % 2 == 0
 
 
     @staticmethod
