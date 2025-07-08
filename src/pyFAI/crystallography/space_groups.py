353aa801f4b9c163065332f133f39386921feb0b
Gudrun Lotze <64900988+gudlot@users.noreply.github.com>
2025-07-08 18:26:40 +0200

    Validated group44_Imm2

--- src/pyFAI/crystallography/space_groups.py
+++ src/pyFAI/crystallography/space_groups.py
@@ -611,15 +611,27 @@ class ReflectionCondition:
 
 
     @staticmethod
-    def group44_pmna(h, k, l):
-        """Space group 44: Pmna. (0 0 l): l even; (0 k 0): k even; (h 0 0): h even."""
-        if h == 0 and k == 0:
-            return l % 2 == 0
-        if h == 0 and l == 0:
-            return k % 2 == 0
-        if k == 0 and l == 0:
-            return h % 2 == 0
-        return True
+    def group44_Imm2(h, k, l):
+        """
+        Space group 44: Imm2. I-centering.
+        Valid reflections must satisfy:
+        - General (hkl):       h + k + l even
+        - 0kl (h=0):           k + l even
+        - h0l (k=0):           h + l even
+        - hk0 (l=0):           h + k even
+        - h00 (k=0, l=0):      h even
+        - 0k0 (h=0, l=0):      k even
+        - 00l (h=0, k=0):      l even
+        validated
+        """
+        if h == k == 0: return l % 2 == 0  # 00l
+        if h == l == 0: return k % 2 == 0  # 0k0
+        if k == l == 0: return h % 2 == 0  # h00
+        if h == 0: return (k + l) % 2 == 0  # 0kl
+        if k == 0: return (h + l) % 2 == 0  # h0l
+        if l == 0: return (h + k) % 2 == 0  # hk0
+        return (h + k + l) % 2 == 0  # general hkl
+
 
     @staticmethod
     def group45_pcca(h, k, l):
