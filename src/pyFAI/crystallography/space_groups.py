7f32433200d94984b7cf7848d019a3d4012e1d61
Gudrun Lotze <64900988+gudlot@users.noreply.github.com>
2025-07-09 19:32:24 +0200

    Update in group74_Imma

--- src/pyFAI/crystallography/space_groups.py
+++ src/pyFAI/crystallography/space_groups.py
@@ -1356,16 +1356,16 @@ class ReflectionCondition:
 
     @staticmethod
     def group74_Imma(h, k, l):
-        """
+       """
         Space group 74: Imma. Body-centered lattice (I-centering).
         Valid reflections must satisfy:
-        - hkl:         h + k + l even
-        - 0kl:         k + l even
-        - h0l:         h + l even
-        - hk0:         h, k even
-        - h00:         h even
-        - 0k0:         k even
-        - 00l:         l even
+        - general hkl:         h + k + l even
+        - 0kl (h=0):           k + l even
+        - h0l (k=0):           h + l even
+        - hk0 (l=0):           h, k even
+        - h00 (k=0, l=0):      h even
+        - 0k0 (h=0, l=0):      k even
+        - 00l (h=0, k=0):      l even
         validated
         """
         if (h + k + l) % 2 != 0: # general
