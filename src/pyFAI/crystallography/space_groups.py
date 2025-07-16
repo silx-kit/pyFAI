ffbb9b8c1243c842f9cc8d244829ae70567ddcea
Gudrun Lotze <64900988+gudlot@users.noreply.github.com>
2025-07-16 21:04:49 +0200

    Validated SG 109

--- src/pyFAI/crystallography/space_groups.py
+++ src/pyFAI/crystallography/space_groups.py
@@ -2029,11 +2029,33 @@ class ReflectionCondition:
         return True
 
     @staticmethod
-    def group109_p42ccm(h, k, l):
-        """Space group 109: P42ccm. (0,0,l): l even."""
-        if h == 0 and k == 0:
-            return l % 2 == 0
-        return True
+    def group109_I41md(h, k, l):
+        """
+        Space group 109: I4₁md. Tetragonal. I-centering.
+        Valid reflections must satisfy:
+        - hkl:            h + k + l even (I-centering)
+        - hk0 (l=0):      h + k even
+        - 0kl (h=0):      k + l even
+        - hhl (h=k):      2h + l= 4n
+        - 00l (h=0,k=0):  l= 4n
+        - h00 (k=0,l=0):  h even
+        - hh0 (h=k,l=0):  h even
+        validated
+        """
+        if (h + k + l) % 2 != 0:  # I-centering (hkl)
+            return False
+        if h == 0 and k == 0:  # 00l
+            return l % 4 == 0
+        if l == 0:  # l=0 cases
+            if h == k:  # hh0
+                return h % 2 == 0
+            return (h + k) % 2 == 0  # hk0
+        if h == 0:  # 0kl
+            return (k + l) % 2 == 0
+        if h == k:  # hhl
+            return (2 * h + l) % 4 == 0
+        return True  
+    
 
     @staticmethod
     def group110_p42ncm(h, k, l):
