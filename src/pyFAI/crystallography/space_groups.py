58ab4662e387ab42d1a507b1f4132b1aa5e7620c
Gudrun Lotze <64900988+gudlot@users.noreply.github.com>
2025-08-05 12:10:01 +0200

    Validated SG 185

--- src/pyFAI/crystallography/space_groups.py
+++ src/pyFAI/crystallography/space_groups.py
@@ -3737,11 +3737,39 @@ class ReflectionCondition:
 
 
     @staticmethod
-    def group185_p6_2_22(h, k, l):
-        """Space group 185: P6_222. (0,0,l): l = 3n."""
+    def group185_P63cm(h: int, k: int, l: int) -> bool:
+        """
+        Space group 185: P6₃cm. Hexagonal system, primitive lattice.
+        Valid reflections must satisfy:
+        - 000l (h = 0, k = 0):              l = 2n
+        - h0l (k = 0):                      l = 2n
+        - 0kl (h = 0):                      l = 2n
+        - h(-h)0l (k = -h):                 l = 2n
+
+        Source:
+            Reflection conditions from ITC (in hkil), adapted to (h, k, l)
+            using the relation i = -(h + k).
+            JKC: http://img.chem.ucl.ac.uk/sgp/large/185az2.htm
+            validated
+        """
+        # 000l condition
         if h == 0 and k == 0:
-            return l % 3 == 0
+            return l % 2 == 0
+
+        # h0l condition
+        if k == 0:
+            return l % 2 == 0
+
+        # 0kl condition
+        if h == 0:
+            return l % 2 == 0
+
+        # h(-h)0l condition
+        if k == -h:
+            return l % 2 == 0
+
         return True
+
 
     @staticmethod
     def group186_p6_4_22(h, k, l):
