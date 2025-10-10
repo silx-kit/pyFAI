c8b36a49b36b871cac3c8cee78d62300b1aef928
Gudrun Lotze <64900988+gudlot@users.noreply.github.com>
2025-10-10 09:41:06 +0200

    Added orientation to __repr__ in _Lambda

--- src/pyFAI/detectors/_xspectrum.py
+++ src/pyFAI/detectors/_xspectrum.py
@@ -69,6 +69,8 @@ class _Lambda(Detector):
 
     def __repr__(self):
         txt = f"Detector {self.name}\t PixelSize= {to_eng(self._pixel1)}m, {to_eng(self._pixel2)}m"
+        if self.orientation:
+            txt+=f"\t {self.orientation.name} ({self.orientation.value})"
         if self.sensor:
             txt += f"\t {self.sensor}"
         return txt
