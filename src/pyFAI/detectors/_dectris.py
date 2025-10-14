c177a4300698d7744e8d72494d3047cb4db4daf9
Gudrun Lotze <64900988+gudlot@users.noreply.github.com>
2025-10-14 22:55:32 +0200

    Added sensor to set_config in Pilatus

--- src/pyFAI/detectors/_dectris.py
+++ src/pyFAI/detectors/_dectris.py
@@ -663,6 +663,10 @@ class Pilatus(_Dectris):
 
         # pixel size is enforced by the detector itself
         self._orientation = Orientation(config.get("orientation", 0))
+
+        if "sensor" in config:
+            self.sensor = SensorConfig(config["sensor"])
+
         if "max_shape" in config:
             self.max_shape = tuple(config["max_shape"])
         module_size = config.get("module_size")
