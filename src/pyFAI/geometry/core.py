c463d9469926a81224d5c509b8efc04924708a44
Jérôme Kieffer <jerome.kieffer@terre-adelie.org>
2025-11-11 14:46:27 +0100

    compatibility with fit2d

--- src/pyFAI/geometry/core.py
+++ src/pyFAI/geometry/core.py
@@ -40,7 +40,7 @@ __author__ = "Jérôme Kieffer"
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "08/11/2025"
+__date__ = "11/11/2025"
 __status__ = "production"
 __docformat__ = "restructuredtext"
 
@@ -1772,7 +1772,9 @@ class Geometry:
         """
         with self._sem:
             f2d = convert_to_Fit2d(self)
-        return f2d._asdict()
+            dico = f2d._asdict()
+            dico["splinefile"] = dico.get("splineFile")
+        return dico
 
     @deprecated_args({"splinefile":"splineFile"}, since_version="2025.10")
     def setFit2D(
