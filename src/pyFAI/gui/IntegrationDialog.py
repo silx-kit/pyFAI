39187acf1d5178339ccbcdc89db53ba277f1e26e
Edgar <edgar.gutierrez-fernandez@esrf.fr>
2025-12-12 14:20:19 +0100

    display Integrate1D fiber

--- src/pyFAI/gui/IntegrationDialog.py
+++ src/pyFAI/gui/IntegrationDialog.py
@@ -116,7 +116,7 @@ class IntegrationProcess(qt.QDialog, integrate.IntegrationObserver):
 
     def __displayResult(self, result, resetZoom=False):
         self._plot.clear()
-        if isinstance(result, containers.Integrate1dResult):
+        if isinstance(result, (containers.Integrate1dResult, containers.Integrate1dFiberResult)):
             self._plot.setGraphXLabel("Radial")
             self._plot.setGraphYLabel("Intensity")
             self._plot.addHistogram(
@@ -126,6 +126,8 @@ class IntegrationProcess(qt.QDialog, integrate.IntegrationObserver):
                 color="blue",
                 histogram=result.intensity,
                 resetzoom=False)
+            if isinstance(result, containers.Integrate1dFiberResult):
+                self._plot.setGraphXLabel("Integrated (in-plane / out-of-plane)")
         elif isinstance(result, containers.Integrate2dResult):
 
             def computeLocation(result):
