857a9145a2f8e38bd88aee10b520decfc0a1f2c0
Jerome Kieffer <jerome.kieffer@esrf.fr>
2026-01-13 18:15:17 +0100

    Ruff reformat

--- src/pyFAI/detectors/multi_module.py
+++ src/pyFAI/detectors/multi_module.py
@@ -51,10 +51,14 @@ from ..io.ponifile import PoniFile
 from ..third_party.classproperties import classproperty
 
 
-module_d = numpy.dtype([('d0', numpy.float64),
-                        ('d1', numpy.float64),
-                        ('ring', numpy.int32),
-                        ('module', numpy.int32)])
+module_d = numpy.dtype(
+    [
+        ("d0", numpy.float64),
+        ("d1", numpy.float64),
+        ("ring", numpy.int32),
+        ("module", numpy.int32),
+    ]
+)
 
 
 # Those are the optimizable parameters ... 2 translations and one rotation.
@@ -74,13 +78,14 @@ class ModuleParam:
     def nb_param(cls):
         return len(cls.__dataclass_fields__)
 
+
 @dataclass
 class PoniParam:
-    dist:float=0.0
-    poni1:float=0.0
-    poni2:float=0.0
-    rot1:float=0.0
-    rot2:float=0.0
+    dist: float = 0.0
+    poni1: float = 0.0
+    poni2: float = 0.0
+    rot1: float = 0.0
+    rot2: float = 0.0
     # rot3:float=0.0
     # wavelength:float=0.0
 
@@ -93,7 +98,7 @@ class SingleModule:
     def __init__(self, detector, mask, index=None, fixed=False):
         self.parent_detector = detector
         self.parent_index = index
-        if (index is not None) and index<=mask.max():
+        if (index is not None) and index <= mask.max():
             self.mask = mask == index
         else:
             self.mask = mask
@@ -104,7 +109,10 @@ class SingleModule:
         self.calc_bounding_box()
 
     def __repr__(self):
-         return f"Module centered at ({self.center[0,0]:.1f}, {self.center[1,0]:.1f})"+(", fixed." if self.fixed else ".")
+        return (
+            f"Module centered at ({self.center[0, 0]:.1f}, {self.center[1, 0]:.1f})"
+            + (", fixed." if self.fixed else ".")
+        )
 
     def calc_bounding_box(self):
         d0, d1 = numpy.where(self.mask)
@@ -112,8 +120,7 @@ class SingleModule:
         d0M = d0.max()
         d1m = d1.min()
         d1M = d1.max()
-        self.center =  numpy.atleast_2d([0.5 * (d0M + d0m + 1),
-                                    0.5 * (d1M + d1m + 1)]).T
+        self.center = numpy.atleast_2d([0.5 * (d0M + d0m + 1), 0.5 * (d1M + d1m + 1)]).T
         self.bounding_box = (slice(d0m, d0M + 1), slice(d1m, d1M + 1))
         return self.bounding_box
 
@@ -132,15 +139,18 @@ class SingleModule:
 
         param = param or self.param
 
-        mpc = numpy.vstack((mp1.ravel(),
-                            mp2.ravel()))
+        mpc = numpy.vstack((mp1.ravel(), mp2.ravel()))
         if not self.fixed:
             self.center
             mpc -= self.center
             rot = param.rot
             c, s = cos(rot), sin(rot)
-            rotm = numpy.array([[c, -s],[s,c]])
-            mpc = numpy.dot(rotm, mpc) + self.center + numpy.atleast_2d([param.d0, param.d1]).T
+            rotm = numpy.array([[c, -s], [s, c]])
+            mpc = (
+                numpy.dot(rotm, mpc)
+                + self.center
+                + numpy.atleast_2d([param.d0, param.d1]).T
+            )
         if full_detector:
             mshape = mp1.shape
             p1[self.mask] = mpc[0].reshape(mshape)
@@ -156,6 +166,7 @@ class SingleModule:
 
 class MultiModule:
     """Split a detector in several modules"""
+
     def __init__(self):
         self.modules = {}  # this is contains all of modules
         self.lmask = None
@@ -163,10 +174,14 @@ class MultiModule:
         self.nb_modules = 0
 
     def __repr__(self):
-        return f"MultiModule with {self.nb_modules} modules:\n"+"\n".join(f"  {i:2d}: {j}" for i,j in self.modules.items())
+        return f"MultiModule with {self.nb_modules} modules:\n" + "\n".join(
+            f"  {i:2d}: {j}" for i, j in self.modules.items()
+        )
 
     def build_labels(self):
-        self.lmask, self.nb_modules =  ndimage.label(numpy.logical_not(self.detector.mask))
+        self.lmask, self.nb_modules = ndimage.label(
+            numpy.logical_not(self.detector.mask)
+        )
 
     @classmethod
     def from_detector(cls, detector):
@@ -178,7 +193,7 @@ class MultiModule:
             raise RuntimeError("`detector` must provide an actual mask")
         self.detector = detector
         self.build_labels()
-        for l in range(1, self.nb_modules+1):  # noqa: E741
+        for l in range(1, self.nb_modules + 1):  # noqa: E741
             self.modules[l] = SingleModule(detector, self.lmask, index=l, fixed=False)
         return self
 
@@ -191,7 +206,7 @@ class MultiModule:
         p1 /= self.detector.pixel1
         p2 /= self.detector.pixel2
 
-        for l in range(1, self.nb_modules+1):  # noqa: E741
+        for l in range(1, self.nb_modules + 1):  # noqa: E741
             m = self.modules[l]
             mp1, mp2 = m.calc_displacement_map()
             p1[m.mask] = mp1[m.mask]
@@ -202,6 +217,7 @@ class MultiModule:
     @property
     def free_modules(self):
         return sum(not m.fixed for m in self.modules.values())
+
 
 class MultiModuleRefinement(MultiModule):
     def __init__(self):
@@ -218,13 +234,19 @@ class MultiModuleRefinement(MultiModule):
         p2 = mcp.d1.copy()
         param_idx = 0
         center = 0.5 if center else 0
-        for l in range(1, self.nb_modules+1):  # noqa: E741
+        for l in range(1, self.nb_modules + 1):  # noqa: E741
             m = self.modules[l]
             mask = mcp.module == l
             valid = mcp[mask]
-            sub_param = None if param is None or m.fixed else ModuleParam(*param[3*param_idx:3*(param_idx+1)])
+            sub_param = (
+                None
+                if param is None or m.fixed
+                else ModuleParam(*param[3 * param_idx : 3 * (param_idx + 1)])
+            )
             param_idx += 0 if m.fixed else 1
-            mp1, mp2 = m.calc_position(d1=valid.d0+center, d2=valid.d1+center, param=sub_param)
+            mp1, mp2 = m.calc_position(
+                d1=valid.d0 + center, d2=valid.d1 + center, param=sub_param
+            )
             p1[mask] = mp1
             p2[mask] = mp2
         return p1, p2
@@ -235,8 +257,8 @@ class MultiModuleRefinement(MultiModule):
         else:
             print(filename, ":", self.calibrants.get(filename))
             modulated_cp = self.modulated_points[filename]
-            for l in range(1, self.nb_modules+1):   # noqa: E741
-                print(l, (modulated_cp.module==l).sum())
+            for l in range(1, self.nb_modules + 1):  # noqa: E741
+                print(l, (modulated_cp.module == l).sum())
 
     def load_control_points(self, filename, poni=None, verbose=False):
         """
@@ -248,7 +270,7 @@ class MultiModuleRefinement(MultiModule):
         self.calibrants[filename] = cp.calibrant
         if poni:
             self.ponis[filename] = PoniFile(poni)
-        #build modulated list of control points
+        # build modulated list of control points
         d0 = []
         d1 = []
         ring = []
@@ -259,7 +281,9 @@ class MultiModuleRefinement(MultiModule):
             ring.append(i[2])
             modules.append(0)
         modulated_cp = numpy.rec.fromarrays((d0, d1, ring, modules), dtype=module_d)
-        linear = numpy.round(modulated_cp.d0).astype(numpy.int32) * self.shape[-1] + numpy.round(modulated_cp.d1).astype(numpy.int32)
+        linear = numpy.round(modulated_cp.d0).astype(numpy.int32) * self.shape[
+            -1
+        ] + numpy.round(modulated_cp.d1).astype(numpy.int32)
         modulated_cp.module = self.lmask.ravel()[linear]
         self.modulated_points[filename] = modulated_cp
         if verbose:
@@ -267,42 +291,54 @@ class MultiModuleRefinement(MultiModule):
 
     def init_q_theo(self, force=False):
         if force or not self._q_theo:
-            self._q_theo = {key: 20.0 * pi / numpy.array(calibrant.dspacing)[self.modulated_points[key].ring]
-                            for key, calibrant in self.calibrants.items()}
+            self._q_theo = {
+                key: 20.0
+                * pi
+                / numpy.array(calibrant.dspacing)[self.modulated_points[key].ring]
+                for key, calibrant in self.calibrants.items()
+            }
 
     def residu(self, param=None):
         """Calculate the delta_q value between the expected ring position and the actual one"""
         if not self._q_theo:
             self.init_q_theo()
-        module_param = param[:ModuleParam.nb_param*sum(not m.fixed for m in self.modules.values())]
+        module_param = param[
+            : ModuleParam.nb_param * sum(not m.fixed for m in self.modules.values())
+        ]
         delta = []
-        for (idx, (key, calibrant)) in enumerate(self.calibrants.items()):
+        for idx, (key, calibrant) in enumerate(self.calibrants.items()):
             # print(key)
-            tmp_e = self._q_theo[key]  # This is the theoritical q_value for the given ring (in nm^-1)
+            tmp_e = self._q_theo[
+                key
+            ]  # This is the theoritical q_value for the given ring (in nm^-1)
             # print("exp", len(tmp_e), tmp_e)
             dp1, dp2 = self.calc_cp_positions(param=module_param, key=key)
             # print("dp", len(dp1), len(dp2))
 
-            start_idx = ModuleParam.nb_param*self.free_modules + idx*PoniParam.nb_param
+            start_idx = (
+                ModuleParam.nb_param * self.free_modules + idx * PoniParam.nb_param
+            )
             end_idx = start_idx + PoniParam.nb_param
             poni_param = PoniParam(*param[start_idx:end_idx])
             # print(poni_param)
-            tmp_c = _geometry.calc_q(poni_param.dist,
-                                          poni_param.rot1,
-                                          poni_param.rot2,
-                                          0.0,
-                                          dp1 - poni_param.poni1,
-                                          dp2 - poni_param.poni2,
-                                          calibrant.wavelength)
+            tmp_c = _geometry.calc_q(
+                poni_param.dist,
+                poni_param.rot1,
+                poni_param.rot2,
+                0.0,
+                dp1 - poni_param.poni1,
+                dp2 - poni_param.poni2,
+                calibrant.wavelength,
+            )
             # print("residu", tmp_e, tmp_c)
-            delta.append(tmp_c-tmp_e)
+            delta.append(tmp_c - tmp_e)
         return numpy.concatenate(delta)
 
     @property
     def nb_param(self):
         """Number of parameters for the refinement"""
         free = sum(not m.fixed for m in self.modules.values())
-        return free*ModuleParam.nb_param + PoniParam.nb_param*len(self.calibrants)
+        return free * ModuleParam.nb_param + PoniParam.nb_param * len(self.calibrants)
 
     def init_param(self):
         """Generate the numpy array with all parameters"""
@@ -324,25 +360,24 @@ class MultiModuleRefinement(MultiModule):
         idx = 0
         for i, m in self.modules.items():
             if m.fixed:
-                print(f'module #{i:2d}: Fixed')
+                print(f"module #{i:2d}: Fixed")
             else:
-                res = f'module #{i:2d}:'
+                res = f"module #{i:2d}:"
                 for i, n in enumerate(ModuleParam.__dataclass_fields__, start=idx):
                     res += f" {n:5s}= {param[i]},"
                 idx += ModuleParam.nb_param
                 print(res)
         for p in self.ponis:
-            res = f'{p}:'
+            res = f"{p}:"
             for i, n in enumerate(PoniParam.__dataclass_fields__, start=idx):
                 res += f" {n:5s}= {param[i]:6f},"
             print(res)
             idx += PoniParam.nb_param
-
 
     def cost(self, param):
         delta = self.residu(param)
         return numpy.dot(delta, delta)
 
     def refine(self, param, method="SLSQP"):
-        method = 'Nelder-Mead' if method.lower() == "simplex" else method
+        method = "Nelder-Mead" if method.lower() == "simplex" else method
         return optimize.minimize(self.cost, param, method=method)
