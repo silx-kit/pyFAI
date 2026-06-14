12cdc314df38f130006b0a5c51aa032ce51b7c74
Jerome Kieffer <jerome.kieffer@esrf.fr>
2026-06-14 09:55:59 +0200

    rerun tutorials

--- doc/source/usage/rebuild_tutorials.py
+++ doc/source/usage/rebuild_tutorials.py
@@ -1,51 +1,70 @@
 #!/usr/bin/env python3
 
+"""Tool to re-run all notebooks in the git repo in prevision of a release
+"""
+__author__ = "Jerome Kieffer"
+__date__ = "14/06/2026"
+
 import time
 import os
 import json
 import sys
 import subprocess
+from datetime import timedelta
 
-t0 = time.perf_counter()
-notebooks = []
-interactive = {}
-failed = []
 
-usage = os.path.dirname(os.path.abspath(__file__))
-print(usage)
-for root, dirs, files in os.walk(usage):
-    for f in files:
-        if f.endswith(".ipynb") and not f.startswith(".") and not "checkpoint" in f and not "-Copy" in f:
-           notebooks.append(os.path.join(usage,root,f))
+def get_root():
+    return os.path.dirname(os.path.abspath(__file__))
 
-for f in notebooks:
+
+def get_ipynb(root):
+    notebooks = []
+    proc = subprocess.run(["git", "ls-files"], capture_output=True, cwd=root)
+    assert proc.returncode == 0
+    for f in proc.stdout.decode().split():
+        if f.endswith(".ipynb") and "checkpoint" not in f and "-Copy" not in f:
+            notebooks.append(os.path.join(root, f))
+    return notebooks
+
+
+def check_interactive(notebook):
     inter = False
-    with open(f) as fd:
+    with open(notebook) as fd:
         nb = json.load(fd)
     for k in nb["cells"]:
+        if inter:
+            break
         for src in k["source"]:
-           if src.startswith("%matplotlib widget"):
-               inter = True
-    interactive[f] = inter
+            if src.startswith("%matplotlib widget"):
+                inter = True
+                break
+    return inter
 
-for k,v in interactive.items():
-    if v is False:
-        print(f"Rerun notebook {k} ", end="")
-        sys.stdout.flush()
-        t1 = time.perf_counter()
-        proc = subprocess.run([sys.executable, "-m", "nbconvert", "--to", "notebook", "--execute", "--inplace",
-                               k], capture_output=True
-                              )
-        if proc.returncode != 0:
-            failed.append(k)
-            print(f"t={time.perf_counter()-t1:.3f}s FAILED")
-            print(proc.stdout)
-            #rint(proc.stderr)
-        else:
-            print(f"t={time.perf_counter()-t1:.3f}s")
+
+def run_notebook(fn):
+    print(f"Rerun notebook {fn}\t", end="")
+    sys.stdout.flush()
+    t1 = time.perf_counter()
+    proc = subprocess.run([sys.executable, "-m", "nbconvert", "--to", "notebook", "--execute", "--inplace",
+                           fn], capture_output=True)
+    if proc.returncode != 0:
+        print(f"t={timedelta(time.perf_counter()-t1)} FAILED")
+        print(proc.stdout)
+        return True
     else:
-        print(f"Not rerun notebook {f} as interactive")
-print("Failed:\n"+"\n".join(failed))
-print("Interactive:\n"+"\n".join(i for i in interactive if interactive[i] is False))
-print(f"Runtime: {time.perf_counter()-t0:.3f}s")
+            print(f"t={timedelta(time.perf_counter()-t1)}")
+
+
+if __name__ == "__main__":
+    t0 = time.perf_counter()
+    root = get_root()
+    print("Working directory:", root)
+    print("*"*50)
+    interactive = {n:check_interactive(n) for n in get_ipynb(root)}
+    print("Interactive:\n"+"\n".join(i for i in interactive if interactive[i] is True))
+    print("*"*50)
+    failed = [ fn for fn in interactive if not interactive[fn] and run_notebook(fn)]
+    print("Failed:\n"+"\n".join(failed))
+    print("*"*50)
+    print(f"Runtime: {timedelta(seconds=time.perf_counter()-t0)}")
 
